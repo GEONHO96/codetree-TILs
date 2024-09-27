e99b344ab44279c3780c6f316d33ffee57b5e70d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-09-27 17:32:06 +0900

    Title: 입력받아 출력 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 240927/입력받아 출력 2/input-and-output-2.java
+++ 240927/입력받아 출력 2/input-and-output-2.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // 변수 선언 및 출력
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("-");
-        int front = sc.nextInt();
-        int back = sc.nextInt();
-        System.out.println(front + "" + back);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        // 출력
+        System.out.println(a + "" + b);
     }
 }
