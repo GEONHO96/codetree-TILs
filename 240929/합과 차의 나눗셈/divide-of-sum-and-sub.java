bc8b60c45fe7f85466b142c4f9791aff56f2f6b7
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-09-29 11:27:57 +0900

    Title: 합과 차의 나눗셈, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240929/합과 차의 나눗셈/divide-of-sum-and-sub.java
+++ 240929/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -7,6 +7,6 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out.printf("%.2f", (dobule)(a + b) / (a - b));
+        System.out.printf("%.2f", (double)(a + b) / (a - b));
     }
 }
