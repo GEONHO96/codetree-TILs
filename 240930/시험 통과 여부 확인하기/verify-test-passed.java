bc591090dec0f5606c0e476240f83658230d9e48
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-09-30 16:17:23 +0900

    Title: 시험 통과 여부 확인하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240930/시험 통과 여부 확인하기/verify-test-passed.java
+++ 240930/시험 통과 여부 확인하기/verify-test-passed.java
@@ -3,12 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        if (n >= 80) {
+        // 변수 선언
+        int n;
+        
+        // 입력
+        n = sc.nextInt();
+        
+        // 출력
+        if(n >= 80)
             System.out.println("pass");
-        } else {
+        else
             System.out.println((80 - n) + " more score");
-        }
     }
 }
