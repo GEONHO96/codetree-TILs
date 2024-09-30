037ffe96a02298ee4f51af18d1c99c2173d18be0
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-09-30 10:33:18 +0900

    Title: 큰 수에서 빼기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 240930/큰 수에서 빼기/subtract-from-large-number.java
+++ 240930/큰 수에서 빼기/subtract-from-large-number.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        if (a >= b) {
-            System.out.println(a - b);
-        } else if (a < b) {
+        // 변수 선언
+        int a, b;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        // 출력
+        if(a < b)
             System.out.println(b - a);
-        }
+        if(a >= b)
+            System.out.println(a - b);
     }
 }
