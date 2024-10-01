95797a1e870c106d2eb4a0ab633655c471ddfac2
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-01 18:56:48 +0900

    Title: 2개 중 최대, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/2개 중 최대/max-of-two-nums.java
+++ 241001/2개 중 최대/max-of-two-nums.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = a > b ? b : a;
+        int c = a > b ? a : b;
+        System.out.println(c);
     }
 }
