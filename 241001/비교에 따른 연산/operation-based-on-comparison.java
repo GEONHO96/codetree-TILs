8695cd556916e8e2d8148b7ee351f8f4f223b324
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-01 11:31:13 +0900

    Title: 비교에 따른 연산, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 241001/비교에 따른 연산/operation-based-on-comparison.java
+++ 241001/비교에 따른 연산/operation-based-on-comparison.java
@@ -1,12 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
 
-        if (a > b) System.out.println(a * b);
-        else System.out.println(b / a);
+        // 변수 선언
+        int a, b;
+        
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        
+        // 출력
+        if(a > b)
+            System.out.println(a * b);
+        else
+            System.out.println(b / a);
     }
 }
