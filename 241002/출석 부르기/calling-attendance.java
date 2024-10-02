8e9c30aba15169cdb04199f131655155cca705df
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-02 15:10:20 +0900

    Title: 출석 부르기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/출석 부르기/calling-attendance.java
+++ 241002/출석 부르기/calling-attendance.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args ) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        if (number == 1) {
+
+        // 변수 선언
+        int n;
+
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        if(n == 1)
             System.out.println("John");
-        }
-        else if (number == 2) {
+        else if(n == 2)
             System.out.println("Tom");
-        }
-        else if (number == 3) {
+        else if(n == 3)
             System.out.println("Paul");
-        }
-        else {
+        else
             System.out.println("Vacancy");
-        }
     }
 }
