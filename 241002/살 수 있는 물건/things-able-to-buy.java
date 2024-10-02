5b06c09a90d93b84c25f43c4c9ba25d9b9bc0bed
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-02 14:59:11 +0900

    Title: 살 수 있는 물건, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/살 수 있는 물건/things-able-to-buy.java
+++ 241002/살 수 있는 물건/things-able-to-buy.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n == 3000) {
+        if (n >= 3000) {
             System.out.println("book");
+        } else if (n >= 1000 && n < 3000) {
+            System.out.println("mask");
         } else {
             System.out.println("no");
         }
