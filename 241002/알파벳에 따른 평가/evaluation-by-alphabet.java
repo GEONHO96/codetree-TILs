d6ef09ce3d6e0e01d10a75c1a8c6a08e8a8339aa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-02 17:52:06 +0900

    Title: 알파벳에 따른 평가, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 241002/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -3,21 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        char ch = str.charAt(0);
 
-        if (ch == 'S') {
+        // 변수 선언
+        char a;
+
+        // 입력
+        a = sc.next().charAt(0);
+
+        // 출력
+        if(a == 'S')
             System.out.println("Superior");
-        } else if (ch == 'A') {
+        else if(a == 'A')
             System.out.println("Excellent");
-        } else if (ch == 'B') {
+        else if(a == 'B')
             System.out.println("Good");
-        } else if (ch == 'C') {
+        else if(a == 'C')
             System.out.println("Usually");
-        } else if (ch == 'D') {
+        else if(a == 'D')
             System.out.println("Effort");
-        } else {
+        else
             System.out.println("Failure");
-        }
     }
 }
