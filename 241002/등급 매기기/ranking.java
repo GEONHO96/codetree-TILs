87574af0a32c7769ad1bcd026cfdc0893ed6c1c6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-02 17:35:29 +0900

    Title: 등급 매기기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 241002/등급 매기기/ranking.java
+++ 241002/등급 매기기/ranking.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        if (n >= 90) {
+
+        // 변수 선언
+        int n;
+
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        if(n >= 90)
             System.out.println("A");
-        } else if (n >= 80) {
+        else if(n >= 80)
             System.out.println("B");
-        } else if (n >= 70) {
+        else if(n >= 70)
             System.out.println("C");
-        } else if (n >= 60) {
+        else if(n >= 60)
             System.out.println("D");
-        } else {
-            System.out.println("F");
-        }
+        else
+            System.out.println("F");        
     }
 }
