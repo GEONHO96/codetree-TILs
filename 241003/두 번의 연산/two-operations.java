da24cf6f2630fda1c624b75b17e71d63c70fc739
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-03 10:12:09 +0900

    Title: 두 번의 연산, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 241003/두 번의 연산/two-operations.java
+++ 241003/두 번의 연산/two-operations.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a % 2 == 1) {
+
+        // 변수 선언
+        int a;
+
+        // 입력
+        a = sc.nextInt();
+
+        if(a % 2 == 1)
             a += 3;
-        }
-        if (a % 3 == 0) {
+
+        if(a % 3 == 0)
             a /= 3;
-        }
+
         System.out.println(a);
     }
 }
