30959a5848dd0c6409103c76c179ecf203ae956b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-03 14:05:52 +0900

    Title: 특정 조건 두 정수 비교, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241003/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
+++ 241003/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.java
@@ -6,16 +6,16 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        if (a > b) {
-            System.out.println(1);
+        if (a < b) {
+            System.out.print(1 + " ");
         } else {
-            System.out.println(0);
+            System.out.print(0 + " ");
         }
 
         if (a == b) {
-            System.out.println(1);
+            System.out.print(1 + " ");
         } else {
-            System.out.println(0);
+            System.out.print(0 + " ");
         }
     }
 }
