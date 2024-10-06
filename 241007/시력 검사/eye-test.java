5b2479877f822a5f6b9fc3fb9be1f11cff1f8142
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 08:09:47 +0900

    Title: 시력 검사, Time: 131ms, Memory: 11MB, Status: Passed - CodeTree

--- 241007/시력 검사/eye-test.java
+++ 241007/시력 검사/eye-test.java
@@ -6,6 +6,7 @@ public class Main {
         double a = sc.nextDouble();
         double b = sc.nextDouble();
         if (a >= 1.0 && b >= 1.0) System.out.println("High");
+        if ((a >= 1.0 && b < 1.0 && b >= 0.5) || (b >= 1.0 && a < 1.0 && a >= 0.5)) System.out.println("Middle");
         if (a >= 0.5 && a < 1.0 && b >= 0.5 && b < 1.0) System.out.println("Miiddle");
         if (a < 0.5 || b < 0.5) System.out.println("Low");
     }
