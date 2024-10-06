717aecc56c91b14c5bc25c95609a95fc8c90c7bf
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 08:12:13 +0900

    Title: 시력 검사, Time: 143ms, Memory: 11MB, Status: Passed - CodeTree

--- 241007/시력 검사/eye-test.java
+++ 241007/시력 검사/eye-test.java
@@ -3,11 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        if (a >= 1.0 && b >= 1.0) System.out.println("High");
-        if ((a >= 1.0 && b < 1.0 && b >= 0.5) || (b >= 1.0 && a < 1.0 && a >= 0.5)) System.out.println("Middle");
-        if (a >= 0.5 && a < 1.0 && b >= 0.5 && b < 1.0) System.out.println("Miiddle");
-        if (a < 0.5 || b < 0.5) System.out.println("Low");
+
+        // 변수 선언
+        double a, b;
+
+        // 입력
+        a = sc.nextDouble();
+        b = sc.nextDouble();
+
+        if(a >= 1.0 && b >= 1.0)
+            System.out.println("High");
+        else if(a >= 0.5 && b >= 0.5)
+            System.out.println("Middle");
+        else
+            System.out.println("Low");
     }
 }
