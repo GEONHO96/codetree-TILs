977f2633212bad42b88de6a51563df808be51319
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 14:37:21 +0900

    Title: 장학금, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/장학금/scholarship.java
+++ 241007/장학금/scholarship.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int midScore = sc.nextInt();
-        int finalScore = sc.nextInt();
-        if (midScore >= 90 && finalScore >= 95) {
-            System.out.println(100000);
-        }
-        else if (midScore >= 90 && finalScore >= 90) {
-            System.out.println(50000);
-        }
-        else {
-            System.out.println(0);
-        }
+
+        // 변수 선언
+        int m, f;
+
+        // 입력
+        m = sc.nextInt();
+        f = sc.nextInt();
+
+        // 출력
+        if(m >= 90 && f >= 95)
+            System.out.println("100000");
+        else if(m >= 90 && f >= 90)
+            System.out.println("50000");
+        else
+            System.out.println("0");
     }
 }
