e6f0c99ecd102e4df0b1a91b9948318870d46dfe
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 14:04:56 +0900

    Title: 둘 중 하나의 배수, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/둘 중 하나의 배수/multiple-of-either.java
+++ 241007/둘 중 하나의 배수/multiple-of-either.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scaner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         if (a % 3 == 0 || a % 5 == 0) {
             System.out.println(1);
