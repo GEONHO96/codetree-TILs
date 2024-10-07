32888918fabe43f01c8f16f3e4e14522bf107308
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 12:58:55 +0900

    Title: 숫자의 조건 여부 3, Time: 177ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/숫자의 조건 여부 3/numbers-condition-3.java
+++ 241007/숫자의 조건 여부 3/numbers-condition-3.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if (a % 13 == 0 || a % 19 == 0) {
+
+        // 변수 선언
+        int a;
+
+        // 입력
+        a = sc.nextInt();
+
+        // 출력
+        if(a % 13 == 0 || a % 19 == 0)
             System.out.println("True");
-        } else {
+        else
             System.out.println("False");
-        }
     }
 }
