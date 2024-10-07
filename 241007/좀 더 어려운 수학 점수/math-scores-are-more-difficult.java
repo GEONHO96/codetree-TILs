65c1ec014abf55678d0d1bdcf4a45d0415fcf9d3
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-07 15:58:36 +0900

    Title: 좀 더 어려운 수학 점수, Time: 166ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
+++ 241007/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -3,25 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int mathA = sc.nextInt();
-        int englishA = sc.nextInt();
-        int mathB = sc.nextInt();
-        int englishB = sc.nextInt();
 
-        if (mathA > mathB) {
+        // 변수 선언
+        int a_math, a_eng;
+        int b_math, b_eng;
+
+        // 입력
+        a_math = sc.nextInt();
+        a_eng = sc.nextInt();
+        b_math = sc.nextInt();
+        b_eng = sc.nextInt();
+
+        // 출력
+        if(a_math > b_math || (a_math == b_math && a_eng > b_eng))
             System.out.println("A");
-        }
-
-        else if (mathA < mathB) {
+        else
             System.out.println("B");
-        }
-
-        else if (mathA == mathB) {
-            if (englishA > englishB) {
-                System.out.println("A");
-            } else {
-                System.out.println("B");
-            }
-        }
     }
 }
