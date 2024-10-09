c1ef5b45a5b92c292c3fb5cace75fae8f90df107
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-09 11:21:50 +0900

    Title: 윤년인가, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241009/윤년인가/is-leap-year.java
+++ 241009/윤년인가/is-leap-year.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
-        if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0) {
+
+        // 변수 선언
+        int y;
+
+        // 입력
+        y = sc.nextInt();
+
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
             System.out.println("true");
-        } else {
+        else
             System.out.println("false");
-        }
     }
 }
