82576ade0d9505a048400e9f35a2cdfae4d8d8d6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-10 17:28:23 +0900

    Title: 차례로 출력, Time: 100ms, Memory: 8MB, Status: Passed - CodeTree

--- 241010/차례로 출력/print-in-order.java
+++ 241010/차례로 출력/print-in-order.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        for (int i = 5; i <= 17; i++) {
+        // 출력
+        for (int i = 5; i <= 17; i++)
             System.out.print(i + " ");
-        }
     }
 }
