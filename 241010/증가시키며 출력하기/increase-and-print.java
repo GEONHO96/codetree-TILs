805b86436fd32269a87b8d8167836b9801dab598
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-10 20:20:44 +0900

    Title: 증가시키며 출력하기, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 241010/증가시키며 출력하기/increase-and-print.java
+++ 241010/증가시키며 출력하기/increase-and-print.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i = 5; i <= 17; i += 2) {
+        // 출력
+        for(int i = 5; i <= 17; i += 2)
             System.out.print(i + " ");
-        }
     }
 }
