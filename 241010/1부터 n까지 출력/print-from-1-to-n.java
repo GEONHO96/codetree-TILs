9aea7fcc0298c16bcf45a1ac1f51cfd03445ce29
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-10 20:17:02 +0900

    Title: 1부터 n까지 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241010/1부터 n까지 출력/print-from-1-to-n.java
+++ 241010/1부터 n까지 출력/print-from-1-to-n.java
@@ -3,9 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        // 변수 선언
+        int n;
+
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        for(int i = 1; i <= n; i++)
             System.out.print(i + " ");
-        }
     }
 }
