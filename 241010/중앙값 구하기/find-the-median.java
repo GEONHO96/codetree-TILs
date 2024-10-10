c3a095cba556a8ec505e5dbb3f6633d9f5cbdb78
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-10 17:19:21 +0900

    Title: 중앙값 구하기, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 241010/중앙값 구하기/find-the-median.java
+++ 241010/중앙값 구하기/find-the-median.java
@@ -3,34 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        
-        if (a > b && a > c) {
-            if (b > c) {
+
+        // 변수 선언
+        int a, b, c;
+
+        // 입력
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        // 출력
+        if(a > b) {
+            if(c > a) {
+                // a > b, c > a 일때 (c > a > b)
+                System.out.println(a);
+            }
+            // a > b, a > c 일때 (a가 가장 크고, b와 c중 더 큰 수가 중앙값)
+            else if(b > c) {
                 System.out.println(b);
             }
             else {
                 System.out.println(c);
             }
         }
-
-        if (b > c && b > a) {
-            if (c > a) {
-                System.out.println(c);
-            }
-            else {
-                System.out.println(a);
-            }
-        }
-
-        if (c > a && c > b) {
-            if (a > b) {
-                System.out.println(a);
-            }
-            else {
+        else {
+            if(c > b) {
+                // b > a, c > b 일때 (c > b > a)
                 System.out.println(b);
+            }
+            // b > a, b > c 일때 (b가 가장 크고, a와 c중 더 큰 수가 중앙값)
+            else if(a > c) {
+                System.out.println(a);
+            }
+            else {
+                System.out.println(c);
             }
         }
     }
