95680cf1f9b0afcf380a6f8fb4d4d736af610251
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-11 16:55:03 +0900

    Title: 증가시키며 출력하기 3, Time: 96ms, Memory: 8MB, Status: Passed - CodeTree

--- 241011/증가시키며 출력하기 3/increase-and-print-3.java
+++ 241011/증가시키며 출력하기 3/increase-and-print-3.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int n = 10;
-        while (n <= 26);
-        System.out.print(n + " ");
-        n += 2;
+        while (n <= 26) {
+            System.out.print(n + " ");
+            n += 2;
+        }        
     }
 }
