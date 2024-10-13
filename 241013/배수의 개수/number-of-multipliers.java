24d71b9a57998d476863345f92cd0673c9c6ab42
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-13 22:36:24 +0900

    Title: 배수의 개수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/배수의 개수/number-of-multipliers.java
+++ 241013/배수의 개수/number-of-multipliers.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for (int i = 1; i <= 10; i++) {
-            int a = sc.nextInt();
-            if(a % 3 == 0) {
-                cnt1++;
-            }
-            if(a % 5 == 0) {
-                cnt2++;
-            }
+
+        // 변수 선언
+        int a, cnt3 = 0, cnt5 = 0;
+
+        // 입력
+        for(int i = 1; i <= 10; i++) {
+            a = sc.nextInt();
+
+            if(a % 3 == 0)
+                cnt3++;
+            if(a % 5 == 0)
+                cnt5++;
         }
-        System.out.print(cnt1 + " ");
-        System.out.print(cnt2);
+
+        // 출력
+        System.out.print(cnt3 + " " + cnt5);
     }
 }
