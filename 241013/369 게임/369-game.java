57d63049e203957c17c762054382350f6a39f016
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-13 21:58:57 +0900

    Title: 369 게임, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241013/369 게임/369-game.java
+++ 241013/369 게임/369-game.java
@@ -3,15 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int temp = 1;
-        while (n >= temp) {
-            if (temp % 3 == 0 || temp % 10 == 3 || temp % 10 == 6 || temp % 10 == 9 || temp / 10 == 3 || temp / 10 == 6 || temp / 10 == 9) {
-                System.out.print(0 + " ");
-            } else {
-                System.out.print(temp + " ");
-            }           
-            temp++;
+
+        // 변수 선언
+        int n;
+
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        for(int i = 1; i <= n; i++) {
+            if(i % 3 == 0)
+                System.out.print("0 ");
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                System.out.print("0 ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 == 9)
+                System.out.print("0 ");
+            else
+                System.out.print(i + " ");
         }
     }
 }
