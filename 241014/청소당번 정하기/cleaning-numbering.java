aac8d0e8adb4a0ae494ec1a4d73a9272efafedc9
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-14 00:00:26 +0900

    Title: 청소당번 정하기, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241014/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                cnt1++;
+                if (i % 6 == 0) {
+                    cnt1 += 0;
+                }
+                if (i % 12 == 0) {
+                    cnt1 += 0;
+                }
+            }
+            if (i % 3 == 0 || i % 6 == 0) {
+                cnt2++;
+                if (i % 12 == 0) {
+                    cnt2 += 0;
+                }
+            }
+            if (i % 12 == 0) {
+                cnt3++;
+            }            
+        }
+        System.out.print(cnt1 - 1 + " " + cnt2 + " " + cnt3);
+    }
+}
