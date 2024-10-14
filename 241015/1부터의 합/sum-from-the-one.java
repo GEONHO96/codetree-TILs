acc5ad15e2df1c34912d877da78194aaebcb6fac
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 01:30:20 +0900

    Title: 1부터의 합, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241015/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int cnt = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (sum >= n) {
+                break;
+            }
+            sum += i;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
