b3f471fafbead36f5caddff75ae88489d5495a66
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-14 23:00:25 +0900

    Title: 총합 구하기, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/총합 구하기/find-the-total.java
+++ 241014/총합 구하기/find-the-total.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int a, b;
         int sum = 0;
 
-        for (int i = a; i <= b; i++) {
-            if (i % 6 == 0 && i % 8 != 0) {
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // a부터 b까지 조건을 만족하는 수를 더합니다.
+        for(int i = a; i <= b; i++) {
+            if(i % 6 == 0 && i % 8 != 0)
                 sum += i;
-            }
         }
+
         System.out.println(sum);
     }
 }
