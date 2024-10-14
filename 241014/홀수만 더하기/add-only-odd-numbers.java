a99114a21a1614c50c36c30bb9364e7d5730d790
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-14 09:43:34 +0900

    Title: 홀수만 더하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/홀수만 더하기/add-only-odd-numbers.java
+++ 241014/홀수만 더하기/add-only-odd-numbers.java
@@ -3,14 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int sumVal = 0;
-        for (int i = 1; i <= n; i++) {
-            int num = sc.nextInt();
-            if(num % 2 == 1 && num % 3 == 0) {
-                sumVal += num;
-            }
+
+        // 변수 선언
+        int n, sum = 0;
+
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            int a;
+            a = sc.nextInt();
+            if(a % 2 == 1 && a % 3 == 0)
+                sum += a;
         }
-        System.out.println(sumVal);
+
+        // 출력
+        System.out.print(sum);
     }
 }
