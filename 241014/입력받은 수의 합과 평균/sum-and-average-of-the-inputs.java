a5b9d9b42ed1afad818d142ef6089d0706c85658
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-14 22:56:43 +0900

    Title: 입력받은 수의 합과 평균, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
+++ 241014/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -3,14 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
+
+        // 합을 구합니다.
         int sum = 0;
-        double avg;
-        for (int i = 1; i <= n; i++) {
-            int num = sc.nextInt();
-            sum += num;
+        for(int i = 0; i < n; i++) {
+            int score;
+            score = sc.nextInt();
+            sum += score;
         }
-        avg = (double) sum / n;
+
+        // 평균을 구합니다.
+        double avg = (double)sum / n;
+
         System.out.printf("%d %.1f", sum, avg);
     }
 }
