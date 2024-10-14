3ce31d1ebde228454e64f455d202b6d33dd2486f
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 01:33:32 +0900

    Title: 1부터의 합, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/1부터의 합/sum-from-the-one.java
+++ 241015/1부터의 합/sum-from-the-one.java
@@ -13,7 +13,7 @@ public class Main {
         // 1부터 증가시키며 더한 값이 n이상이 된 순간, 더해진 숫자를 출력합니다.
         for(int i = 1; i <= 100; i++) {
             sum += i;
-            if(sum > n) {
+            if(sum >= n) {
                 System.out.print(i);
                 break;
             }
