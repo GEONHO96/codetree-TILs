5af4c2ddc8bd78f8a297ef8681f534d347f84eb3
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-14 10:11:19 +0900

    Title: 숫자 사이의 배수, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241014/숫자 사이의 배수/multiplication-between-numbers.java
+++ 241014/숫자 사이의 배수/multiplication-between-numbers.java
@@ -3,19 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = 0;
-        int cnt = 0;
 
-        for (int i = a; i <= b; i++) {
-            if (i % 5 == 0 || i % 7 == 0) {
-                cnt++;
+        // 변수 선언 및 입력
+        int a, b;
+        int sum = 0, cnt = 0;
+        double avg;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        // a부터 b까지의 수 중 5 또는 7의 배수인 수들을 더합니다.
+        for(int i = a; i <= b; i++) {
+            if(i % 5 == 0 || i % 7 == 0) {
                 sum += i;
+                cnt++;
             }
         }
-        double avg = (double) sum / cnt;
-        System.out.print(sum + " ");
-        System.out.printf("%.1f", avg);
+
+        // a부터 b까지의 수 중 5 또는 7의 배수인 수들의 평균을 구합니다.
+        avg = (double) sum / cnt;
+
+        // 출력
+        System.out.printf("%d %.1f", sum, avg);
     }
 }
