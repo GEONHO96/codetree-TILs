79264d9ae7ad93d7f636484b798f1cd081ede4d4
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 18:45:09 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.java
+++ 241015/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            int width = sc.nextInt();
-            int length = sc.nextInt();
-            char ch = sc.next().charAt(0);
-            System.out.println(width * length);
-            if (ch == 'C') {
+
+        while(true) {
+            // 변수 선언 및 입력
+            int w, h;
+            char a;
+            w = sc.nextInt();
+            h = sc.nextInt();
+            a = sc.next().charAt(0);
+
+            // 사각형의 넓이를 출력합니다. 문자 C가 입력되면 종료합니다.
+            System.out.println(w * h);
+
+            if(a == 'C')
                 break;
-            }
         }
     }
 }
