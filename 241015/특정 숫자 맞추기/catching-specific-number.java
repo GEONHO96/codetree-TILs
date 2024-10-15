393bf7a5d662405dd649bb5060778248c2f1c58a
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 14:44:06 +0900

    Title: 특정 숫자 맞추기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/특정 숫자 맞추기/catching-specific-number.java
+++ 241015/특정 숫자 맞추기/catching-specific-number.java
@@ -4,16 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            int num = sc.nextInt();
+        while(true) {
+            // 변수 선언 및 입력
+            int n;
+            n = sc.nextInt();
 
-            if (num > 25) {
+            // n이 25보다 작으면 Higher을, 25보다 크면 Lower을, 25와 같으면 Good을 출력한 뒤 종료합니다.
+            if(n < 25)
+                System.out.println("Higher");
+
+            else if(n > 25)
                 System.out.println("Lower");
-            } else if (num == 25) {
+
+            else {
                 System.out.println("Good");
                 break;
-            } else {
-                System.out.println("Higher");
             }
         }
     }
