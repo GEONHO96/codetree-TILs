981fafd85ab7be974a246b5381b8710b947f244b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 19:22:37 +0900

    Title: 3n + 1 수열, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/3n + 1 수열/3n-plus-1-sequence.java
+++ 241015/3n + 1 수열/3n-plus-1-sequence.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
         int cnt = 0;
+
         while(true) {
-            if(N % 2 == 0) {
-                N /= 2;
-                cnt++;
-            } else if(N % 2 == 1 && N != 1) {
-                N = 3 * N + 1;
-                cnt++;
-            }
-            if(N == 1) {
+            // n이 1이 될 때까지 홀수일 때는 n = 3n+1을, 짝수일 때는 n = n/2를 반복합니다.
+            if(n == 1)
                 break;
-            }            
+
+            if(n % 2 == 1)
+                n = 3 * n + 1;
+            else
+                n = n / 2;
+            
+            cnt++;
         }
+
         System.out.println(cnt);
     }
 }
