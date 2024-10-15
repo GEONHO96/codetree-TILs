f299d8eb6a76f9f29c0909fd7dbde844b793aff6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-15 13:43:18 +0900

    Title: 1부터 곱셈하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241015/1부터 곱셈하기/multiple-from-one.java
+++ 241015/1부터 곱셈하기/multiple-from-one.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        // 변수 선언
+        int n;
         int prod = 1;
-        for (int i = 2; i <= 11; i++) {
-            if (prod >= n) {
-                System.out.println(i - 1);
-                break;
-            }              
+
+        n = sc.nextInt();
+
+        // 1부터 증가시키며 곱한 값이 n이상이 된 순간, 곱해진 숫자를 출력합니다.
+        for(int i = 1; i <= 10; i++) {
             prod *= i;
-        }
+            if(prod >= n) {
+                System.out.print(i);
+                break;
+            }
+        }            
     }
 }
