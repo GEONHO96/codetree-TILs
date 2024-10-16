2bbded805bfb83770bb640dbd26121ab54d441f8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-16 17:00:34 +0900

    Title: 합성수 판별기, Time: 158ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241016/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 2; i <= n - 1; i++) {
+            if (n % i == 0) {
+                System.out.println("C");
+                break;
+            }
+            else {
+                if (i == n - 1 && i == 2) {
+                    System.out.println("N");
+                }
+            }
+        }
+    }
+}
