e5dc14677b744835648f321b7aa8810c73aaea5f
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-16 17:23:37 +0900

    Title: ab 사이에 있는 c, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/ab 사이에 있는 c/c-between-a-and-b.java
+++ 241016/ab 사이에 있는 c/c-between-a-and-b.java
@@ -3,22 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int a, b, c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
         boolean satisfied = false;
-        
+
         for(int i = a; i <= b; i++) {
-            if (i % c == 0) {
+            // a에서 b사이의 값 중 c의 배수가 있는지 확인합니다.
+            if(i % c == 0)
                 satisfied = true;
-            }
         }
 
-        if (satisfied == true) {
+        //출력
+        if(satisfied == true)
             System.out.println("YES");
-        }
-        else {
+        else
             System.out.println("NO");
-        }
+            
     }
 }
