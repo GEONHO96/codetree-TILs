b7e69d8c6d0aea3fe3a33f03fe0fc18728e72ff9
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-17 08:31:19 +0900

    Title: 소수 판별기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/소수 판별기/prime-number-discriminator.java
+++ 241017/소수 판별기/prime-number-discriminator.java
@@ -12,7 +12,7 @@ public class Main {
         for(int i = 2; i < n; i++) {
             // 1과 자기자신을 제외한 약수가 전혀 없다면 그 수는 소수입니다.
             if(n % i == 0)
-                satisfied = true;
+                satisfied = false;
         }
 
         //출력
