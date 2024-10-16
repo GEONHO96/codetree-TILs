2d013ea494fd1fa54cd4ae2f4108288da90897d4
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-17 08:30:30 +0900

    Title: 소수 판별기, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241017/소수 판별기/prime-number-discriminator.java
+++ 241017/소수 판별기/prime-number-discriminator.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
         boolean satisfied = true;
 
-        for (int i = 2; i <= n - 1; i++) {
-            if (n % i == 0) {
-                satisfied = false;
-            }                        
+        for(int i = 2; i < n; i++) {
+            // 1과 자기자신을 제외한 약수가 전혀 없다면 그 수는 소수입니다.
+            if(n % i == 0)
+                satisfied = true;
         }
 
-        if (satisfied == true) {
+        //출력
+        if(satisfied == true)
             System.out.println("P");
-        } else {
+        else
             System.out.println("C");
-        }
     }
 }
