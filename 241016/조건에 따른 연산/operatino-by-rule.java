ab5d5735f22fb9a6c2824fd15f78e85c05413fac
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-16 13:56:51 +0900

    Title: 조건에 따른 연산, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/조건에 따른 연산/operatino-by-rule.java
+++ 241016/조건에 따른 연산/operatino-by-rule.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
         int cnt = 0;
 
-        while (n < 1000) {
-            if (n % 2 == 1) {
-                n = 2 * n + 2;                
-            } else {
-                n = 3 * n + 1;                
-            }
+        while(true) {
+            // n이 1000이 될 때까지 연산을 반복합니다.
+            if(n >= 1000)
+                break;
+            
+            if(n % 2 == 1)
+                n = 2 * n + 2;
+            else
+                n = 3 * n + 1;
+                
             cnt++;
         }
-        System.out.println(cnt);           
+
+        System.out.println(cnt);
     }
 }
