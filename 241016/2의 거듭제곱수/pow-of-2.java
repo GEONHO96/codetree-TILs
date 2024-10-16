671c5c20e359ed6ee3a4c6feb555385134242560
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-16 09:59:04 +0900

    Title: 2의 거듭제곱수, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/2의 거듭제곱수/pow-of-2.java
+++ 241016/2의 거듭제곱수/pow-of-2.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
+        int prod = 1;
         int x = 0;
 
-        while (N > 0) {
-            N /= 2;
-            if (N == 0) {
+        while(true) {
+            // prod가 n이 될 때까지 2를 곱합니다.
+            if(n == prod)
                 break;
-            }
+            
+            prod *= 2;
             x++;
         }
+
         System.out.println(x);
     }
 }
