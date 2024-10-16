5d1a6519e8fc0d4afa066aabd379d67be8118e67
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-16 17:32:23 +0900

    Title: 합성수 판별기, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

--- 241016/합성수 판별기/factorization-discriminator.java
+++ 241016/합성수 판별기/factorization-discriminator.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+
+        // 변수 선언 및 입력
+        int n;
+        n = sc.nextInt();
         boolean satisfied = false;
 
-        for (int i = 2; i <= n - 1; i++) {
-            if (n % i == 0) {
+        for(int i = 2; i < n; i++) {
+            // n의 약수가 있다면 합성수입니다.
+            if(n % i == 0)
                 satisfied = true;
-            }
         }
 
-        if (satisfied == true) {
+        //출력
+        if(satisfied == true)
             System.out.println("C");
-        }
-        else {
+        else
             System.out.println("N");
-        }
     }
 }
