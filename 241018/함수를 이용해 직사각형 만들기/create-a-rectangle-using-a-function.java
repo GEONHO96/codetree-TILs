ec62a41d05e7b043a704444e8e81dc2cd5cd06fa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-18 00:16:58 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 181ms, Memory: 11MB, Status: Passed - CodeTree

--- 241018/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
+++ 241018/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    
-    public static void printSquare(int n, int m) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                System.out.print(1);
-            }
+    // n * m 크기의 사각형을 출력합니다.
+    public static void printRect(int n, int m) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++)
+                System.out.print("1");
             System.out.println();
         }
     }
-    
+
+
     public static void main(String[] args) {
+        // 변수 선언 및 입력:
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        printSquare(n, m);
+        int rowNum = sc.nextInt();
+        int colNum = sc.nextInt();
+        printRect(rowNum, colNum);
     }
 }
