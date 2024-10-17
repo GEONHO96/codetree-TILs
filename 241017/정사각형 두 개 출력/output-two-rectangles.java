9ee2921f472ce53b089724ba25d1c61f389fbfa5
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-17 09:04:13 +0900

    Title: 정사각형 두 개 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 241017/정사각형 두 개 출력/output-two-rectangles.java
+++ 241017/정사각형 두 개 출력/output-two-rectangles.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        for(int i = 0; i < 2; i++) {
-            for (int j = 0; j < n; j++) {
-                for(int k = 0; k < n; k++) {
-                    System.out.print("*");
-                }
-                System.out.println();
+        // n * n 크기의 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        // n * n 크기의 별을 한번 더 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
