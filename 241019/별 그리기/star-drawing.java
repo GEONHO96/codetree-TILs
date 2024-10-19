52caadedd1a27f2e2a5d9c7b422ab842e8cb9755
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-19 19:14:51 +0900

    Title: 별 그리기, Time: 339ms, Memory: 14MB, Status: Passed - CodeTree

--- 241019/별 그리기/star-drawing.java
+++ 241019/별 그리기/star-drawing.java
@@ -3,29 +3,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n - i; j++) {
+
+        // Step 1:
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= i; j++) {
+            for(int j = 0; j < 2 * i - 1; j++) {
                 System.out.print("*");
             }
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(" ");
             }
-            System.out.println();            
+            System.out.println();
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j <= i; j++) {
+        // Step 2:
+        for(int i = n - 1; i >= 1; i--) {
+            for(int j = 0; j < n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j < n - i; j++) {
+            for(int j = 0; j < 2 * i - 1; j++) {
                 System.out.print("*");
             }
-            for (int j = 1; j < n - i - 1; j++) {
-                System.out.print("*");
+            for(int j = 0; j < n - i; j++) {
+                System.out.print(" ");
             }
             System.out.println();
         }
