b2b75da001c30aef27fc08019bd91794e2ca757a
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-19 20:26:27 +0900

    Title: 체크 다이아몬드, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 241019/체크 다이아몬드/check-diamond.java
+++ 241019/체크 다이아몬드/check-diamond.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n - i; j++) {
+        // 모양에 맞게 위쪽 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++)
                 System.out.print(" ");
-            }
-            for (int j = 0; j <= i; j++) {
-                System.out.print("* ");                
-            }   
-            System.out.println();                                
+            for(int j = 0; j < i + 1; j++)
+                System.out.print("* ");
+            System.out.println();
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j <= i; j++) {
+        // 모양에 맞게 아래쪽 별을 출력합니다.
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < n - i - 1; j++)
                 System.out.print(" ");
-            }
-            for (int j = 1; j < n - i; j++) {
+            for(int j = 0; j < i + 1; j++)
                 System.out.print("* ");
-            }        
-            System.out.println();            
-        }                        
+            System.out.println();
+        }
     }
 }
