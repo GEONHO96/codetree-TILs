a6f83d0cbc7b1adde6bfeded1902dbfeca06e9de
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-19 19:58:37 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241019/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
+++ 241019/특정 규칙에 따른 문자 출력/character-output-according-to-rule.java
@@ -3,22 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j < n - i; j++) {
+        // 모양에 맞게 윗쪽 별을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i - 1; j++)
                 System.out.print("  ");
-            }
-            for (int j = 0; j <= i; j++) {
+            for(int j = 0; j <= i; j++)
                 System.out.print("@ ");
-            }
             System.out.println();
         }
 
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 1; j < n - i; j++) {
+        // 모양에 맞게 아랫쪽 별을 출력합니다.
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j <= i; j++)
                 System.out.print("@ ");
-            }
             System.out.println();
         }
     }
