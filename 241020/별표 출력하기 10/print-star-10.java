c10992bf1762d05dc31852c3e8a0605805017030
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-20 21:04:44 +0900

    Title: 별표 출력하기 10, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241020/별표 출력하기 10/print-star-10.java
+++ 241020/별표 출력하기 10/print-star-10.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
 
-        for (int i = 1; i <= 2 * n; i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < n - (i - 1) / 2; j++) {
+        // i가 짝수인 경우 별을 1 + (i / 2)개, 홀수인 경우 n - (i - 1) / 2개 출력합니다
+        for(int i = 0; i < 2 * n; i++) {
+            if(i % 2 == 0) {
+                for(int k = 0; k < 1 + i / 2; k++)
                     System.out.print("* ");
-                }
-            } else {
-                for (int j = 0; j < 1 + (i / 2); j++) {
+            }
+            else {
+                for(int k = 0; k < n - (i - 1) / 2; k++)
                     System.out.print("* ");
-                }
             }
             System.out.println();
         }
