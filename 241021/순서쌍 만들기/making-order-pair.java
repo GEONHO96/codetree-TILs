77692257d7fef0845ca6d8a34e55b09cb0dba0ea
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-21 07:58:07 +0900

    Title: 순서쌍 만들기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241021/순서쌍 만들기/making-order-pair.java
+++ 241021/순서쌍 만들기/making-order-pair.java
@@ -6,7 +6,7 @@ public class Main {
         int n = sc.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print("(" + n - i + "," + n - i + ") ");
+                System.out.print("(" + (n - i) + "," + (n - j) + ") ");
             }
             System.out.println();
         }
