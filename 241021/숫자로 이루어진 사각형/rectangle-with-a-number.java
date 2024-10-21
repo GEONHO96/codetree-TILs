04f7ada1a1923dd3c42f05c4399d62d7d8bed2f5
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-21 11:03:26 +0900

    Title: 숫자로 이루어진 사각형, Time: 248ms, Memory: 12MB, Status: Passed - CodeTree

--- 241021/숫자로 이루어진 사각형/rectangle-with-a-number.java
+++ 241021/숫자로 이루어진 사각형/rectangle-with-a-number.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void printSquare(int N) {
-        int cnt = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                cnt += 1;
-                if (cnt == 10) {
-                    cnt = 1;
-                }
+    // n * n 크기의 사각형에 숫자를 출력하는 함수입니다.
+    public static void printNum(int n) {
+        int cnt = 1;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
                 System.out.print(cnt + " ");
+                cnt++;
+                if(cnt == 10)
+                    cnt = 1;
             }
             System.out.println();
         }
@@ -18,7 +17,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        printSquare(N);
+
+        // 변수 선언 및 입력:
+        int rowNum = sc.nextInt();
+
+        printNum(rowNum);
+        
     }
 }
