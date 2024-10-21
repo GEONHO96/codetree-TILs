308db41f606dda862d632dc3cb65e8a364b805cb
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-21 09:21:16 +0900

    Title: 1-9 정사각형, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 241021/1-9 정사각형/1-9-square.java
+++ 241021/1-9 정사각형/1-9-square.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 cnt++;
-                if (cnt == 10) {
+                if (cnt % 10 == 0) {
                     System.out.print(1);
                     cnt++;
                 } else {
