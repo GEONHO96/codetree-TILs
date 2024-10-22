c19ade93895c35b5960f5f6042edc9184bfbc31b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-22 15:10:24 +0900

    Title: 숫자 피라미드 3, Time: 190ms, Memory: 11MB, Status: Passed - CodeTree

--- 241022/숫자 피라미드 3/number-pyramid-3.java
+++ 241022/숫자 피라미드 3/number-pyramid-3.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        // 숫자로 이루어진 삼각형을 출력합니다.
+        for(int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print((i * j) + " ");
             }
