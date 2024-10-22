7d10eb2652d0ac2d87ed2076d7655c3827814ab8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-23 08:23:20 +0900

    Title: 모양대로 숫자 출력하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 241023/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.java
+++ 241023/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j < i; j++) {
-                System.out.print("  ");
-            }
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print((n - j - i + 2) + " ");
+
+        // 숫자로 이루어진 삼각형을 출력합니다.
+        for(int i = n; i >= 1; i--) {
+            for(int j = n; j >= 1; j--) {
+                if(j > i)
+                    System.out.print("  ");
+                else
+                    System.out.print(j + " ");
             }
             System.out.println();
         }
