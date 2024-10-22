5b84921b773e63ff39a1c65d5cdd8a9b9745bec5
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-22 09:24:11 +0900

    Title: 좌우 지그재그 출력, Time: 235ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 241022/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 1) {
+                for (int j = 1; j <= n; j++) {
+                    cnt++;
+                    System.out.print(cnt + " ");
+                }
+            } else {
+                for (int j = 1; j <= n; j++) {
+                    System.out.print(cnt + n - j + 1 + " ");
+                }
+                cnt = cnt + n;
+            }
+            System.out.println();
+        }
+    }
+}
