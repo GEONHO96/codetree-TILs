6ff940bdb199be1d92c446f1d8e655411c5fa08b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-23 15:54:24 +0900

    Title: ab곱 n번 구하기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 241023/ab곱 n번 구하기/a-multiple-b-n-times.java
+++ 241023/ab곱 n번 구하기/a-multiple-b-n-times.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        // n회 반복합니다.
+        for(int i = 0; i < n; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int prod = 1;
-            for (int j = a; j <= b; j++) {
-                prod *= j;
-            }
-            System.out.println(prod);
+            int ans = 1;
+
+            // a부터 b까지 전부 곱한 값을 출력합니다.
+            for(int j = a; j <= b; j++)
+                ans *= j;
+            System.out.println(ans);
         }
     }
 }
