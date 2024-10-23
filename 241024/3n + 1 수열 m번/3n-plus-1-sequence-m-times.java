4f4863c1bfca033e0e64fbc4d6c0b806e331cba6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 08:15:49 +0900

    Title: 3n + 1 수열 m번, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 241024/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -3,21 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력
         int m = sc.nextInt();
-
-        for (int i = 1; i <= m; i++) {
+        
+        // m회 반복합니다.
+        for(int i = 0; i < m; i++) {
+            // 변수 선언 및 입력
             int n = sc.nextInt();
-            int cnt = 0;
-            while (n > 1) {
-                if (n % 2 == 0) {
+            int ans = 0;
+            
+            // n이 1이 될 때까지 3n + 1을 반복합니다.
+            while(n != 1) {
+                if (n % 2 == 0)
                     n /= 2;
-                    cnt++;
-                } else {
-                    n = n * 3 + 1;
-                    cnt++;
-                }
+                else 
+                    n = 3 * n + 1;
+                
+                ans++;
             }
-            System.out.println(cnt);
+            
+            System.out.println(ans);
         }
     }
 }
