8485738bac9d7185214f40171fcacf790b4de6aa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 08:47:43 +0900

    Title: 완전수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/완전수/perfect-number.java
+++ 241024/완전수/perfect-number.java
@@ -5,10 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int start = sc.nextInt();
         int end = sc.nextInt();
-        int sum = 0;
         int cnt = 0;
 
         for (int i = start; i <= end; i++) {
+            int sum = 0;
             for (int j = 1; j < i; j++) {
                 if (i % j == 0) {
                     sum += j;
