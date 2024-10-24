3c39fa437bf0effb262f0a8053efd6db0da9d72e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 22:44:47 +0900

    Title: 배열의 특정 위치 찾기, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 241024/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -3,25 +3,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
-        int sum1 = 0;
-        int sum2 = 0;
-        int cnt2 = 0;
 
-        for (int i = 0; i < 10; i++) {
+        // 변수 선언
+        int[] arr = new int[10];
+        int sum2 = 0;
+        int sum3 = 0;
+        int cnt = 0;
+
+        // 10개의 정수를 배열에 입력받아 저장합니다.
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
 
-        for (int i = 1; i < 10; i += 2) {
-            sum1 += arr[i];
+        // 짝수 번째 인덱스에 들어있는 수들의 합과 3의 배수 번째 인덱스에 들어있는 수들의 평균을 구합니다.
+        for(int i = 0; i < 10; i++) {
+            if((i + 1) % 2 == 0)
+                sum2 += arr[i];
+            if((i + 1) % 3 == 0) {
+                sum3 += arr[i];
+                cnt++;
+            }
         }
 
-        for (int i = 2; i < 10; i += 3) {
-            sum2 += arr[i];
-            cnt2++;
-        }
+        // 3의 배수 번째 인덱스에 들어있는 수들의 평균 구하기
+        double avg3 = (double)sum3 / cnt;
 
-        double avg = (double) sum2 / cnt2;
-        System.out.printf("%d %.1f", sum1, avg);
+        // 출력
+        System.out.printf("%d %.1f", sum2, avg3);
     }
 }
