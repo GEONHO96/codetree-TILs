900a9fec4145a0ca8c93f6373ca8437c63f6c8b0
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 22:25:45 +0900

    Title: 특정 위치의 숫자, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/특정 위치의 숫자/numbers-in-specific-location.java
+++ 241024/특정 위치의 숫자/numbers-in-specific-location.java
@@ -3,11 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 정수 배열 선언
         int[] arr = new int[10];
-        for (int i = 0; i < 10; i++) {
+
+        // 10개의 정수를 배열에 입력받아 저장합니다.
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
-        int sum = arr[2] + arr[4] + arr[9];
-        System.out.println(sum);
+
+        // 3번째, 5번쨰, 10번째 인덱스에 저장된 값 들의 합
+        System.out.print(arr[2] + arr[4] + arr[9]);
     }
 }
