600bb6e013bc45ee11eec0e6dec9f692ae3459df
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 21:09:13 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/시험 통과 여부 확인하기 2/verify-test-passed-2.java
+++ 241024/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -3,24 +3,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        int[] num = new int[4];
-        int cnt = 0;
-        for (int i = 1; i <= T; i++) {
+        
+        // 학생 수 n 입력받기
+        int n = sc.nextInt();
+        
+        // 통과한 사람의 수를 나타내는 변수 : passPeople
+        int passPeople = 0;
+        
+        for(int i = 0; i < n; i++) {
+            // 정수 배열 선언
+            int[] arr = new int[4];
             int sum = 0;
-            int avg = 0;
-            for (int j = 0; j < 4; j++) {
-                num[j] = sc.nextInt();
-                sum += num[j];
+            
+            // 4개의 정수를 배열에 입력받아 저장합니다.
+            for(int j = 0; j < 4; j++) {
+                arr[j] = sc.nextInt();
             }
-            avg = sum / 4;
-            if (avg >= 60) {
+            
+            // 4과목의 점수의 합을 구합니다.
+            for(int j = 0; j < 4; j++) {
+                sum += arr[j];
+            }
+            
+            // 평균을 구합니다.
+            double avg = (double)sum / 4;
+            
+            //출력
+            if(avg >= 60) {
                 System.out.println("pass");
-                cnt++;
-            } else {
-                System.out.println("fail");
+                passPeople++;
             }
+            else
+                System.out.println("fail");
         }
-        System.out.println(cnt);
+        
+        // 통과한 사람의 수 출력
+        System.out.println(passPeople);
     }
 }
