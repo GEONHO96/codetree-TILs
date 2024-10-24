8e7247b0c6e6d5dedc5432173b37e05daceeb8aa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 17:55:04 +0900

    Title: 배열 채우기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/배열 채우기/filling-array.java
+++ 241024/배열 채우기/filling-array.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[11];
+
+        // 정수 배열 선언
+        int[] arr = new int[10];
         int cnt = 0;
-        for (int i = 1; i <= 10; i++) {
-            num[i] = sc.nextInt();
-            if (num[i] == 0) break;
+
+        // 10개의 정수를 배열에 입력받으며 0이 있는지 확인합니다.
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] == 0)
+                break;
             cnt++;
         }
-        for (int i = cnt; i >= 1; i--) {
-            System.out.print(num[i] + " ");
+
+        // 0이 입력되기 전까지의 수를 반대 순서로 출력합니다.
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
