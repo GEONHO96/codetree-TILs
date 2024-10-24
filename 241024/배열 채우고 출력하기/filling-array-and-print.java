c6cc0152b65f27822f963a61845396cd26e2b268
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 12:35:20 +0900

    Title: 배열 채우고 출력하기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/배열 채우고 출력하기/filling-array-and-print.java
+++ 241024/배열 채우고 출력하기/filling-array-and-print.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = "";
-        char[] ch = new char[11];
 
-        for (int i = 1; i <= 10; i++) {
-            ch[i] = sc.next().charAt(0);
+        // 크기가 10인 배열을 구현합니다.
+        char[] arr = new char[10];
+
+        // 0부터 9까지의 인덱스에 주어진 문자를 차례대로 입력받습니다.
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.next().charAt(0);
         }
-        for (int i = 1; i <= 10; i++) {
-            str += ch[11 - i];
+
+        // 9부터 0까지의 인덱스에 주어진 문자를 차례대로 출력합니다.
+        for(int i = 9; i >= 0; i--) {
+            System.out.print(arr[i]);
         }
-        System.out.print(str);
     }
 }
