12bad11c9c4e948badbcbf2f935cf68655b47f60
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-24 10:17:28 +0900

    Title: 짝수의 합 n번 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 241024/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
+++ 241024/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.java
@@ -3,17 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+
+        // n회 반복합니다.
+        for(int i = 0; i < n; i++) {
+            // 변수 선언 및 입력
             int a = sc.nextInt();
             int b = sc.nextInt();
-            int sum = 0;
-            for (int j = a; j <= b; j++) {
-                if (j % 2 == 0) {
-                    sum += j;
-                }
-            }
-            System.out.println(sum);
+            int ans = 0;
+
+            // a부터 b까지 짝수를 전부 더한 값을 출력합니다.
+            for(int j = a; j <= b; j++)
+                if(j % 2 == 0)
+                    ans += j;
+
+            System.out.println(ans);
         }
     }
 }
