bf354f9c178100618ba950ffede0e2950c217461
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 14:50:53 +0900

    Title: 배열에서 원소의 특징, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
+++ 241025/배열에서 원소의 특징/characteristics-of-elements-in-an-array.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언
         int[] arr = new int[10];
-        int k = 0;
-        for (int i = 0; i < 10; i++) {
+
+        // 10개의 정수를 배열에 입력받아 저장합니다.
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 0; i < 10; i++) {
-            if (arr[i] % 3 == 0) {
-                k = i - 1;
+
+        // 값을 하나씩 살펴보며 3의 배수가 처음으로 등장하는 부분을 찾고, 바로 앞 원소를 출력합니다.
+        for(int i = 0; i < 10; i++)
+            if(arr[i] % 3 == 0) {
+                System.out.print(arr[i - 1]);
                 break;
             }
-        }
-        System.out.println(arr[k]);
     }
 }
