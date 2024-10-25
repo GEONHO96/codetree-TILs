f566129399a6e9ede2c5292e9a0a126940b2939d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 18:38:13 +0900

    Title: 십의 자리 숫자, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/십의 자리 숫자/number-of-tens-digit.java
+++ 241025/십의 자리 숫자/number-of-tens-digit.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
+        int[] arr = new int[100];
         int[] count = new int[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             arr[i] = sc.nextInt();
-            count[arr[i] / 10]++;
-            if (arr[i] == 0) break;
+            if (arr[i] >= 10) {
+                count[arr[i] / 10]++;
+            }
+            if (arr[i] == 0) {
+                break;
+            }
         }
         for (int i = 1; i <= 9; i++) {
             System.out.println(i + " - " + count[i]);
