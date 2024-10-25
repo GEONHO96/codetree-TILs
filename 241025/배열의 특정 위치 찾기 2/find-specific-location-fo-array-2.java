dcd8bfc3f8c56e837d6c91a5405d7959dbc95bbf
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 09:39:17 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 164ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.java
+++ 241025/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언
         int[] arr = new int[10];
-        int evenSum = 0;
-        int oddSum = 0;
-        for (int i = 0; i < 10; i++) {
+        
+        int sum1 = 0;
+        int sum2 = 0;
+        
+        // 10개의 정수를 배열에 입력받아 저장합니다.
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            if (i % 2 == 0) {
-                oddSum += arr[i];
-            } else {
-                evenSum += arr[i];
-            }
         }
-        int sub = Math.abs(oddSum - evenSum);
-        System.out.println(sub);
+        
+        // 10개의 정수 중 홀수번째 수들의 합과 짝수번째 수들의 합을 구합니다.
+        for(int i = 0; i < 10; i++) {
+            if(i % 2 == 0)
+                sum1 += arr[i];
+            else 
+                sum2 += arr[i];
+        }
+        
+        // sum1 과 sum2 의 크기를 비교하여 큰 쪽에서 작은쪽을 뺍니다.
+        if(sum1 > sum2)
+            System.out.print(sum1 - sum2);
+        else 
+            System.out.print(sum2 - sum1);
     }
 }
