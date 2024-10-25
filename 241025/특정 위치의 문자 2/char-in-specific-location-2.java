5e91362ec8898e7f8ae5a3de65fda742e7228c08
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 09:24:28 +0900

    Title: 특정 위치의 문자 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/특정 위치의 문자 2/char-in-specific-location-2.java
+++ 241025/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char[] ch = new char[10];
-        String str = "";
-        for (int i = 0; i < 10; i++) {
-            ch[i] = sc.next().charAt(0);
+
+        // 문자 배열 선언
+        char[] arr = new char[10];
+
+        // 10개의 정수를 배열에 입력받아 저장합니다.
+        for(int i = 0; i < 10; i++) {
+            arr[i] = sc.next().charAt(0);
         }
-        str = ch[1] + " " + ch[4] + " " + ch[7];
-        System.out.print(str);
+
+        // 1번째, 4번째, 7번째 인덱스에 저장된 값 들의 합
+        System.out.print(arr[1] + " " + arr[4] + " " + arr[7]);
     }
 }
