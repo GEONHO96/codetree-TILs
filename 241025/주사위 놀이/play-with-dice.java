5d071dee261c06ebceebd1e69cb49380426049d2
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 18:10:40 +0900

    Title: 주사위 놀이, Time: 201ms, Memory: 10MB, Status: Passed - CodeTree

--- 241025/주사위 놀이/play-with-dice.java
+++ 241025/주사위 놀이/play-with-dice.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] count = new int[7];
+
+        // 변수 선언
+        int[] countArr = new int[7];
         int[] arr = new int[10];
-        for (int i = 0; i < 10; i++) {
+
+        // arr[i]를 입력받은 후 카운팅 배열을 통해 각각의 빈도 저장
+        for(int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            count[arr[i]]++;
+            countArr[arr[i]]++;
         }
-        for (int i = 1; i <= 6; i++) {
-            System.out.println(i + " - " + count[i]);
+
+        // 1부터 6까지 나온 횟수를 출력
+        for(int i = 1; i <= 6; i++) {
+            System.out.println(i + " - " + countArr[i]);
         }
     }
 }
