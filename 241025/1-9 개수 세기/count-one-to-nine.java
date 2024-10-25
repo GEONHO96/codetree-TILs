a7ab80c9695f5d12b84985f40b199db7ae73509b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-25 17:02:44 +0900

    Title: 1-9 개수 세기, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241025/1-9 개수 세기/count-one-to-nine.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int[] count = new int[10];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            count[arr[i]]++;
+        }
+        for (int i = 1; i < 10; i++) {
+            System.out.println(count[i]);
+        }
+    }
+}
