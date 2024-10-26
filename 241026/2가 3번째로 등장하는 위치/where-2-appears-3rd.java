c52e6d580a91f072a9e7d34cc4a918cc8df69037
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-26 18:26:32 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 241026/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
+++ 241026/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -3,16 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 정수 n과 배열을 입력받습니다.
         int n = sc.nextInt();
-        int[] arr = new int[n + 1];
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
+        int[] arr = new int[100];
+        for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-            if (arr[i] == 2) {
+
+        // 2가 나온 횟수를 카운트해 줍니다.
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == 2)
                 cnt++;
-            }
-            if (cnt == 3 && arr[i] == 2) {
-                System.out.print(i);
+
+            // 2가 3번째로 등장할 때 그 위치를 출력합니다.
+            if(cnt == 3) {
+                System.out.print(i + 1);
+                break;
             }
         }
     }
