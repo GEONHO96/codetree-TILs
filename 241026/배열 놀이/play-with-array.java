dbc1039fd7c8cdf989e68730d9d1a6a460b49818
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-26 16:53:16 +0900

    Title: 배열 놀이, Time: 148ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241026/배열 놀이/play-with-array.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n + 1];
+        int[] question = new int[q + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 1; i <= q; i++) {
+            question[i] = sc.nextInt();
+            if (question[i] == 1) {
+                int a = sc.nextInt();
+                System.out.println(arr[a]);
+            }
+            if (question[i] == 2) {
+                int b = sc.nextInt();
+                for (int j = 1; j <= n; j++) {
+                    if (arr[j] == b) {
+                        System.out.println(j);
+                        break;
+                    }
+                }
+            }
+            if (question[i] == 3) {
+                int s = sc.nextInt();
+                int e = sc.nextInt();
+                for (int j = s; j <= e; j++) {
+                    System.out.print(arr[j] + " ");
+                }
+            }
+        }
+    }
+}
