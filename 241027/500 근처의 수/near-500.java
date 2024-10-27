1eda52fa313410b8509e73f4ddd94792f763c89f
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-27 16:00:05 +0900

    Title: 500 근처의 수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241027/500 근처의 수/near-500.java
+++ 241027/500 근처의 수/near-500.java
@@ -3,26 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 배열을 구현합니다.
         int[] arr = new int[10];
-        int x = 0;
-        int y = 0;
-        int min = 1000;
-        int max = 1;
-        for (int i = 0; i < 10; i++) {
+        
+        // 10개의 정수를 입력합니다.
+        for(int i = 0; i < 10; i++)
             arr[i] = sc.nextInt();
-            if (arr[i] >= 500) {
-                x = arr[i];
-                if (min > x) {
-                    min = x;
-                }
-            }
-            if (arr[i] < 500) {
-                y = arr[i];
-                if (max < y) {
-                    max = y;
-                }
-            }
+        
+        // maxVal : 500 미만의 가장 큰 수, minVal : 500 초과의 가장 작은 수
+        int maxVal = 1;
+        int minVal = 1000;
+        
+        // 10개의 숫자들 중 500근처의 수들을 구합니다.
+        for(int i = 0; i < 10; i++) {
+            if(arr[i] < 500 && arr[i] > maxVal)
+                maxVal = arr[i];
+            
+            if(arr[i] > 500 && arr[i] < minVal)
+                minVal = arr[i];
         }
-        System.out.printf("%d %d", max, min);
+        
+        // 구한 값을 출력합니다.
+        System.out.print(maxVal + " " + minVal);
     }
 }
