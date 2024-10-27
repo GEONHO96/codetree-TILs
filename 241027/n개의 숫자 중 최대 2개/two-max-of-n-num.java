647bb799b396b784783b95d2f65c20f1ef2002bf
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-27 14:36:20 +0900

    Title: n개의 숫자 중 최대 2개, Time: 306ms, Memory: 16MB, Status: Passed - CodeTree

--- 241027/n개의 숫자 중 최대 2개/two-max-of-n-num.java
+++ 241027/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -1,44 +1,25 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
         // 변수 선언:
-        int[] A = new int[100];
-        int n = 0, max1 = 0, max2 = 0;
+        int n;
         
         // 입력:
         n = sc.nextInt();
+        int[] A = new int[n];
         for(int i = 0; i < n; i++)
             A[i] = sc.nextInt();
         
-        // Step 1: 처음 2개의 원소 중 더 큰 값을 max1에
-        //                       더 작은 값을 max2에 넣습니다.
-        if(A[0] > A[1]) {
-            max1 = A[0];
-            max2 = A[1];
-        }
-        else{
-            max1 = A[1];
-            max2 = A[0];
-        }
+        // 내림차순으로 정렬합니다.
+        Integer[] A2 = Arrays.stream(A).boxed().toArray(Integer[]::new);
+        Arrays.sort(A2, Collections.reverseOrder());
         
-        // Step 2: 3번째 원소부터 보면서 max1과 max2를 갱신합니다.
-        for (int i = 2; i < n; i++) {
-            if (A[i] >= max1) {
-                // Case 1: 지금까지 본 숫자들보다 좋다면
-                //         max2, max1 모두 갱신해줍니다.
-                max2 = max1;
-                max1 = A[i];
-            }
-            else if (A[i] > max2) {
-                // Case 2: max2보다만 좋다면 max2를 갱신합니다.
-                max2 = A[i];
-            }
-        }
-        
-        // 출력:
-        System.out.print(max1 + " " + max2);
+        //  출력:
+        System.out.print(A2[0] + " " + A2[1]);
     }
 }
