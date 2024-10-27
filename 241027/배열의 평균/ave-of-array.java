28f411681a99229ec0182aaa5b647e3fb930f8a2
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-27 20:48:07 +0900

    Title: 배열의 평균, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241027/배열의 평균/ave-of-array.java
+++ 241027/배열의 평균/ave-of-array.java
@@ -3,42 +3,40 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[2][4];
-        int allSum = 0;
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-
-        for (int i = 0; i < 2; i++) {
-            int sum = 0;
-            for (int j = 0; j < 4; j++) {
-                sum += arr[i][j];
-            }
-            double avg = (double) sum / 4;
-            System.out.print(avg + " ");
-        }
-        System.out.println();
-
-        for (int i = 0; i < 4; i++) {
-            int sum = 0;
-            for (int j = 0; j < 2; j++) {
-                sum += arr[j][i];
-            }
-            double avg = (double) sum / 2;
-            System.out.print(avg + " ");
+        
+        // 2차원 배열을 구현합니다.
+        int[][] arr2d = new int[2][4];
+        int sum = 0;
+        
+        // 각 줄마다 정수를 입력받습니다.
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 4; j++)
+                arr2d[i][j] = sc.nextInt();
+        
+        // 가로 평균을 출력합니다.
+        for(int i = 0; i < 2; i++) {
+            sum = 0;
+            for(int j = 0; j < 4; j++)
+                sum += arr2d[i][j];
+                System.out.printf("%1.f ", (double)sum / 4);
         }
         System.out.println();
-
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 4; j++) {
-                allSum += arr[i][j];
-            }
+        
+        // 세로 평균을 출력합니다.
+        for(int j = 0; j < 4; j++) {
+            sum = 0;
+            for(int i = 0; i < 2; i++)
+                sum += arr2d[i][j];
+            System.out.printf("%.1f ", (double)sum / 2);
         }
-
-        double allAvg = (double) allSum / 8;
-        System.out.printf("%.1f", allAvg);
+        System.out.println();
+        
+        // 전체 평균을 출력합니다.
+        sum = 0;
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 4; j++)
+                sum += arr2d[i][j];
+        }
+        System.out.printf("%.1f", (double)sum / 8);
     }
 }
