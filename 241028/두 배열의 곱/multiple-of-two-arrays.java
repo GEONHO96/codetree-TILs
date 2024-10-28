4cd0636007862ef567c8d169e7d86b7c239c0caa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-28 11:45:48 +0900

    Title: 두 배열의 곱, Time: 147ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241028/두 배열의 곱/multiple-of-two-arrays.java
+++ 241028/두 배열의 곱/multiple-of-two-arrays.java
@@ -25,7 +25,6 @@ public class Main {
                 mul[i][j] = arr1[i][j] * arr2[i][j];
                 System.out.print(mul[i][j] + " ");
             }
-            System.out.println();
         }
     }
 }
