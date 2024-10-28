459ded635cbd060fdbed6cb3c49082e87f0ee17b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-28 18:09:10 +0900

    Title: 두 개의 격자 비교하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 241028/두 개의 격자 비교하기/compare-two-grid.java
+++ 241028/두 개의 격자 비교하기/compare-two-grid.java
@@ -10,7 +10,7 @@ public class Main {
         int[][] arr3 = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 arr1[i][j] = sc.nextInt();
             }
         }
