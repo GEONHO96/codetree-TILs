2c00753f74f4d0f931a87175bcecd8e99d2ded8c
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 07:58:26 +0900

    Title: 격자로 사각형 만들기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/격자로 사각형 만들기/print-grid-in-rectangle.java
+++ 241029/격자로 사각형 만들기/print-grid-in-rectangle.java
@@ -3,15 +3,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // n을 입력받습니다.
         int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < n; i++) {
+
+        // 2차원 배열을 구현합니다.
+        int[][] arr = new int[10][10];
+
+        // 배열의 각 행의 첫 열과 마지막 열을 1로 초기화합니다.
+        for(int i = 0; i < n; i++) {
+            arr[i][0] = 1;
+            arr[0][i] = 1;
+        }
+
+        // 배열의 숫자를 채웁니다.
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < n; j++)
+                arr[i][j] = arr[i][j - 1] + arr[i - 1][j] + arr[i - 1][j - 1];
+
+        // 채워진 배열을 출력합니다.
+        for(int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0 || j == 0) {
-                    arr[i][j] = 1;
-                } else {
-                    arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j] + arr[i][j - 1];
-                }
                 System.out.print(arr[i][j] + " ");
             }
             System.out.println();
