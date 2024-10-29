2ec1df83941af4bd0d83a89a5ac248e6c0e6368d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 11:01:59 +0900

    Title: 가장 짧은 문자열, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/가장 짧은 문자열/shortest-string.java
+++ 241029/가장 짧은 문자열/shortest-string.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int max = 0;
         int min = 20;
 
         for (int i = 0; i < 3; i++) {
             String str = sc.next();
             int len = str.length();
+            max = Math.max(max, len);
             min = Math.min(min, len);
         }
 
-        System.out.println(min);
+        System.out.println(max - min);
     }
 }
