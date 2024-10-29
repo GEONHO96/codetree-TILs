9c52c92af1f455e3ea0c65336ccb5bb280ccd23d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 17:26:37 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
+++ 241029/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = new String[]{ "apple", "banana", "grape", "blueberry", "orange" };
-        char ch = sc.next().charAt(0);
+
+        // 문자열 리스트를 정의합니다.
+        String[] str = new String[]{ "apple", "banana", "grape", "blueberry", "orange" };
+
+        // 영문자를 입력받습니다.
+        char a = sc.next().charAt(0);
         int cnt = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (ch == arr[i].charAt(2) || ch == arr[i].charAt(3)) {
+
+        // 조건을 만족하는 문자열을 출력하고 조건을 만족하는 문자열의 개수를 셉니다.
+        for(int i = 0; i < 5; i++)
+            if(str[i].charAt(2) == a || str[i].charAt(3) == a) {
+                System.out.println(str[i]);
                 cnt++;
-                System.out.println(arr[i]);
             }
-        }
+
+        // 조건을 만족하는 문자열의 개수를 출력합니다.
         System.out.println(cnt);
     }
 }
