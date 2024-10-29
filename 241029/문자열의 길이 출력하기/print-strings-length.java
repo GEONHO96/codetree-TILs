622517a110796749abec74d0d8f7e1476849cd33
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 10:16:54 +0900

    Title: 문자열의 길이 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241029/문자열의 길이 출력하기/print-strings-length.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i = 0; i < 2; i++) {
+            String str = sc.next();
+            sum += str.length();
+        }
+        System.out.println(sum);
+    }
+}
