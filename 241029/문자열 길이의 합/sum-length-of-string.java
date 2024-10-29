2382d82a629f46152460f111d9cfa3edcb362a5d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 21:02:35 +0900

    Title: 문자열 길이의 합, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/문자열 길이의 합/sum-length-of-string.java
+++ 241029/문자열 길이의 합/sum-length-of-string.java
@@ -3,15 +3,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // n을 입력받습니다.
         int n = sc.nextInt();
-        String[] str = new String[n];
-        int sum = 0;
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
+        
+        // 문자열 리스트를 정의합니다.
+        String[] str = new String[10];
+        
+        // 문자열을 입력받습니다.
+        for(int i = 0; i < n; i++)
             str[i] = sc.next();
-            if (str[i].charAt(0) == 'a') cnt++;
-            sum += str[i].length();
+        
+        // lenAll : 전체 문자열의 길이, cnt : 첫 번째 문자로 'a'가 나오는 횟수
+        int lenAll = 0;
+        int cnt = 0;
+        
+        // 전체 문자열의 길이와 첫번째 문자로 'a'가 나오는 횟수를 구합니다.
+        for(int i=  0; i < n; i++) {
+            lenAll += str[i].length();
+            if(str[i].charAt(0) == 'a')
+                cnt++;
         }
-        System.out.printf("%d %d", sum, cnt);
+        
+        // 문제에서 구하고자 하는 값들을 출력합니다.
+        System.out.print(lenAll + " " + cnt);
     }
 }
