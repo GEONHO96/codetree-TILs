a7f10ec4224b0e2b12d8153f5ad5d94521845406
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 21:47:21 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 127ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241029/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[10];
+        for (int i = 0; i < 10; i++) {
+            str[i] = sc.next();
+        }
+        char ch1 = sc.next().charAt(0);
+        for (int i = 0; i < 10; i++) {
+            char ch2 = str[i].charAt(str[i].length() - 1);
+            if (ch1 == ch2) {
+                System.out.println(str[i]);
+            }
+        }
+    }
+}
