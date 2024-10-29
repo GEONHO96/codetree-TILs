cbc41d9610bdee78417430eb90b00372cfa71e2a
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 10:18:56 +0900

    Title: 문자열의 길이 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241029/문자열의 길이 출력하기/print-strings-length.java
+++ 241029/문자열의 길이 출력하기/print-strings-length.java
@@ -3,11 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        for (int i = 0; i < 2; i++) {
-            String str = sc.next();
-            sum += str.length();
-        }
-        System.out.println(sum);
+
+        // 문자열을 정의합니다.
+        String str1;
+        String str2;
+
+        // 문자열을 입력받습니다.
+        str1 = sc.next();
+        str2 = sc.next();
+
+        // 문자열의 길이를 구합니다.
+        int len1 = str.length();
+        int len2 = str.length();
+
+        // 두 문자열의 길이의 합을 출력합니다.
+        System.out.println(len1 + len2);
     }
 }
