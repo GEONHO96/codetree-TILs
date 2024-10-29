b32b567624319f392df1518dd8074ea5473b6555
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 22:28:51 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241029/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        int sum = 0;
+        String[] str = new String[n];
+        for (int i = 0; i < 4; i++) {
+            str[i] = sc.next();
+        }
+        char ch = sc.next().charAt(0);
+        for (int i = 0; i < 4; i++) {
+            if (ch == str[i].charAt(0)) {
+                cnt++;
+                sum += str[i].length();
+            }
+        }
+        System.out.printf("%d %.2f", cnt, (double) sum / cnt);
+    }
+}
