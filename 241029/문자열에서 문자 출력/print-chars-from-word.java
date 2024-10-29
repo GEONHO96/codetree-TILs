635272f496b3626d573d8edaf7e7cdb729e4961a
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 22:39:21 +0900

    Title: 문자열에서 문자 출력, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/문자열에서 문자 출력/print-chars-from-word.java
+++ 241029/문자열에서 문자 출력/print-chars-from-word.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-            System.out.println(ch);
+
+        // 문자열을 정의합니다.
+        String str;
+
+        // 문자열을 입력받습니다.
+        str = sc.next();
+
+        // 문자열을 순회하여 문자를 한 줄에 하나씩 출력합니다.
+        for(int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
         }
     }
 }
