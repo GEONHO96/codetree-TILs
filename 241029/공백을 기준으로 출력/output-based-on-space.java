4df1392d4898ab90bcceddc7f08dc26d9be5b3d4
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-29 18:34:07 +0900

    Title: 공백을 기준으로 출력, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 241029/공백을 기준으로 출력/output-based-on-space.java
+++ 241029/공백을 기준으로 출력/output-based-on-space.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = sc.next();
-        String str3 = sc.next();
-        String str4 = sc.next();
-        String str5 = sc.next();
-        String str = str1 + str2 + str3 + str4 + str5;
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        String str = str1 + str2;
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
+            if (Character.isSpaceChar(ch)) {
+                continue;
+            }
             System.out.print(ch);
         }
     }
