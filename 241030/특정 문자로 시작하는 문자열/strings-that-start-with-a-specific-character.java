9c1ca34bc4a8476d770ece55562dc2e339694500
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 13:51:56 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
+++ 241030/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -3,21 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 문자열 리스트를 정의합니다.
+        String[] str = new String[20];
+        
+        // 입력
         int n = sc.nextInt();
-        int cnt = 0;
-        int sum = 0;
-        String[] str = new String[n];
-        for (int i = 0; i < n; i++) {
+        
+        for(int i = 0; i < n; i++)
             str[i] = sc.next();
-        }
-        char ch = sc.next().charAt(0);
-        for (int i = 0; i < n; i++) {
-            if (ch == str[i].charAt(0)) {
+        
+        char a = sc.next().charAt(0);
+        int lenSum = 0;
+        int cnt = 0;
+        
+        // 조건에 맞는 문자열의 총 길이와 개수를 계산해줍니다.
+        for(int i = 0; i < n; i++) {
+            if(str[i].charAt(0) == a) {
+                int len = str[i].length();
+                lenSum += len;
                 cnt++;
-                sum += str[i].length();
             }
         }
-        double avg = (double) sum / cnt;
-        System.out.printf("%d %.2f", cnt, avg);
+        
+        // 구하고자 하는 값을 출력합니다.
+        System.out.printf("%d %.2f", cnt, (double)lenSum / cnt);
     }
 }
