5c83d3cad5b3e2faad1530da4a071fed583e4977
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 21:30:44 +0900

    Title: a로 채워넣기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/a로 채워넣기/filling-with-a.java
+++ 241030/a로 채워넣기/filling-with-a.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 문자열을 입력받습니다.
         String str = sc.next();
-        int length = str.length();
-        for (int i = 0; i < length; i++) {
-            char ch = str.charAt(i);
-            if (i == 1 || i == length - 2) ch = 'a';
-            System.out.print(ch);
-        }
+
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+
+        // 문자열의 앞에서 두 번째 원소와 뒤에서 두 번째 원소를 'a'로 대체합니다.
+        str = str.substring(0, 1) + "a" + str.substring(2);
+        str = str.substring(0, len - 2) + "a" + str.substring(len - 1);
+
+        // 대체된 이후의 문자열을 출력합니다.
+        System.out.println(str);
     }
 }
