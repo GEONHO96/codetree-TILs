4f026b65463b1a431c3150da67f1eba675ea3928
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 10:01:30 +0900

    Title: 문자열 나누기, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/문자열 나누기/divide-string.java
+++ 241030/문자열 나누기/divide-string.java
@@ -3,18 +3,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 문자열을 정의합니다.
+        String[] str = new String[10];
+        String strAll = "";
+
+        // n을 입력받습니다.
         int n = sc.nextInt();
-        String[] str = new String[n];
-        String str2 = "";
-        for (int i = 0; i < n; i++) {
+
+        // 공백 단위로 문자열을 입력받습니다.
+        for(int i = 0; i < n; i++)
             str[i] = sc.next();
-            str2 += str[i];
-        }
-        char[] ch = new char[str2.length()];
-        for (int i = 0; i < str2.length(); i++) {
-            ch[i] = str2.charAt(i);
-            if (i % 5 != 4) System.out.print(ch[i]);
-            else System.out.println(ch[i]);
+
+        // 문자열을 전부 붙입니다.
+        for(int i = 0; i < n; i++)
+            strAll += str[i];
+
+        // 합쳐진 문자열의 길이를 구합니다.
+        int len = strAll.length();
+
+        // 합쳐진 문자열을 5개의 숫자마다 개행을 하여 출력합니다.
+        for(int i = 0; i < len; i++) {
+            System.out.print(strAll.charAt(i));
+            if((i + 1) % 5 == 0)
+                System.out.println();
         }
     }
 }
