75adb291a4065b552574b84c947c4bdaeabbec74
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 11:12:25 +0900

    Title: 특정 문자의 유무, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/특정 문자의 유무/specific-character-presence.java
+++ 241030/특정 문자의 유무/specific-character-presence.java
@@ -3,36 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 문자열을 입력받습니다.
         String str = sc.next();
-        char[] ch = new char[str.length() + 1];
-        boolean exists1 = false;
-        boolean exists2 = false;
 
-        for (int i = 0; i < str.length(); i++) {
-            ch[i] = str.charAt(i);
-        }
-
-        for (int i = 0; i <= str.length(); i++) {
-            if (ch[i] == 'e' && ch[i + 1] == 'e') {
-                exists1 = true;
-            }
-        }
-
-        for (int i = 0; i <= str.length(); i++) {
-            if (ch[i] == 'a' && ch[i + 1] == 'b') {
-                exists2 = true;
-            }
-        }
-
-        if (exists1 == true) {
+        // 문자열 'ee'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
+        if(str.contains("ee")) {
             System.out.print("Yes ");
-        } else {
+        }
+        else {
             System.out.print("No ");
         }
 
-        if (exists2 == true) {
-            System.out.print("Yes ");
-        } else {
+        // 문자열 'ab'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
+        if(str.contains("ab")) {
+            System.out.print("Yes");
+        }
+        else {
             System.out.print("No ");
         }
     }
