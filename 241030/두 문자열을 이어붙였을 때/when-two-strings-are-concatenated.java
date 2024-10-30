649665ccf1d1140dfc862ee3f26549f8b94c9f2b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 10:05:20 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241030/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        String C = A + B;
+        String D = B + A;
+        if (C.equals(D)) System.out.println("true");
+        else System.out.println("false");
+    }
+}
