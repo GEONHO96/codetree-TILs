24dd05549f9cea7effa89cae45c8a3d317ac8851
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 20:13:54 +0900

    Title: 부분문자열 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241030/부분문자열 위치 구하기/find-location-of-substring.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        int length1 = str1.length();
+        int length2 = str2.length();
+        int cnt = -1;
+        for (int i = 0; i < length1; i++) {
+            if ((str1.substring(i, i + length2)).equals(str2.substring(0, length2))) {
+                cnt = i;
+                break;
+            }
+        }
+        if (cnt == -1) {
+            System.out.println(-1);
+        } else {
+            System.out.println(cnt);
+        }
+    }
+}
