4bc5f03ab59dc61f359f401a0fc96bad113bbb6c
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 09:20:11 +0900

    Title: 문자열 추가하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/문자열 추가하기/add-spring.java
+++ 241030/문자열 추가하기/add-spring.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = "Hello";
-        String str = str1 + str2;
-        System.out.print(str);
+
+        // 문자열을 정의합니다.
+        String str;
+        String hel = "Hello";
+
+        // 문자열을 입력받습니다.
+        str = sc.next();
+
+        // 문자열의 뒤에 Hello를 추가합니다.
+        str += hel;
+
+        // 합쳐진 문자열을 출력합니다.
+        System.out.println(str);
     }
 }
