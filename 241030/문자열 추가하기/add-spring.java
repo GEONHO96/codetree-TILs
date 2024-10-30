889fd91896a2caf7030e50e2d1de98fd006bd48a
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 09:17:53 +0900

    Title: 문자열 추가하기, Time: 212ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241030/문자열 추가하기/add-spring.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = "Hello";
+        String str = str1 + str2;
+        System.out.print(str);
+    }
+}
