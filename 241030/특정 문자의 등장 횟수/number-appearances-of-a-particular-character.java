3440c3e8735b72b8258ea955686e3531a330142d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-30 14:21:54 +0900

    Title: 특정 문자의 등장 횟수, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
+++ 241030/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.java
@@ -3,28 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 문자열을 입력받습니다.
         String str = sc.next();
-        int length = str.length();
-        char[] ch = new char[length + 1];
+
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
         int cnt1 = 0;
         int cnt2 = 0;
 
-        for (int i = 0; i < length; i++) {
-            ch[i] = str.charAt(i);
-        }
-
-        for (int i = 0; i < length; i++) {
-            if (ch[i] == 'e' && ch[i + 1] == 'e') {
+        // 문자열 'ee'와 'eb'가 몇 번 나왔는지 확인합니다.
+        for(int i = 0; i < len - 1; i++) {
+            if(str.charAt(i) == 'e' && str.charAt(i + 1) == 'e')
                 cnt1++;
-            }
-        }
-
-        for (int i = 0; i < length; i++) {
-            if (ch[i] == 'e' && ch[i + 1] == 'b') {
+            if(str.charAt(i) == 'e' && str.charAt(i + 1) == 'b')
                 cnt2++;
-            }
         }
 
-        System.out.printf("%d %d", cnt1, cnt2);
+        // 문자열 'ee'와 'eb'가 각각 몇 번 나왔는지 출력합니다.
+        System.out.print(cnt1 + " " + cnt2);
     }
 }
