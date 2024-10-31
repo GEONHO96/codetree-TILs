8c3e1eb7f0af8ef7e7610f6df00c529f538c3053
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 08:25:40 +0900

    Title: 알파벳 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241101/알파벳 지우기/remove-alphabet.java
+++ 241101/알파벳 지우기/remove-alphabet.java
@@ -3,22 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str1 = sc.next();
-        String str2 = sc.next();
-        int length1 = str1.length();
-        int length2 = str2.length();
-        String str3 = "";
-        String str4 = "";
-        for (int i = 0; i < length1; i++) {
-            if (str1.charAt(i) >= '0' && str1.charAt(i) <= '9') {
-                str3 += str1.charAt(i);
-            }
-        }
-        for (int i = 0; i < length2; i++) {
-            if (str2.charAt(i) >= '0' && str2.charAt(i) <= '9') {
-                str4 += str2.charAt(i);
-            }
-        }
-        System.out.println(Integer.parseInt(str3) + Integer.parseInt(str4));
+
+        // 문자열을 정의합니다.
+        String a;
+        String b;
+        String str1 = "";
+        String str2 = "";
+
+        // 문자열을 입력받습니다.
+        a = sc.next();
+        b = sc.next();
+
+        // 문자열의 길이를 구합니다.
+        int lenA = a.length();
+        int lenB = b.length();
+        int cnt1 = 0;
+        int cnt2 = 0;
+
+        // a의 정수로 변환 가능한 부분을 다른 문자열로 옮깁니다.
+        for(int i = 0; i < lenA; i++)
+            if(a.charAt(i) <= '9' && b.charAt(i) >= '0')
+                str1 += a.charAt(i);
+
+        // b의 정수로 변환 가능한 부분을 다른 문자열을 옮깁니다.
+        for(int i = 0; i < lenB; i++)
+            if(b.charAt(i) <= '9' && b.charAt(i) >= '0')
+                str2 += b.charAt(i);
+
+        // 합쳐진 문자열을 숫자로 바꿉니다.
+        int str1Int = Integer.parseInt(str1);
+        int str2Int = Integer.parseInt(str2);
+        
+        // 두 숫자의 합을 출력합니다.
+        System.out.print(str1Int + str2Int);
     }
 }
