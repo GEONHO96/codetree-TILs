e8c544d822c00912bbf83e2f368d79c80f4b5799
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 14:05:46 +0900

    Title: 문자열 돌리기, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/문자열 돌리기/SPin-SPring.java
+++ 241031/문자열 돌리기/SPin-SPring.java
@@ -5,9 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int L = str.length();
-        for (int i = 0; i < L; i++) {
-            str = str.substring(L - i) + str.substring(0, L - i);
-            System.out.println(str);
+        for (int i = 0; i <= L; i++) {
+            String str1 = str;
+            str1 = str1.substring(L - i) + str1.substring(0, L - i);
+            System.out.println(str1);
         }
     }
 }
