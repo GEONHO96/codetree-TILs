2ac63c1b992191c34a9903a4301c59bcb1f80e47
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 17:08:55 +0900

    Title: 아스키코드 변환, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/아스키코드 변환/convert-to-ascii.java
+++ 241031/아스키코드 변환/convert-to-ascii.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char a = sc.next().charAt(0);
-        int b = sc.nextInt();
-        System.out.print((int) a + " " + (char) b);
+
+        // 문자와 숫자를 입력받습니다.
+        char x = sc.next().charAt(0);
+        int a = sc.nextInt();
+
+        // 문자와 아스키코드값과 숫자 아스키코드 번호에 해당하는 문자값을 출력합니다.
+        System.out.print((int)x + " " + (char)a);
     }
 }
