7ab79d5a1c436b460f7263c7a50e2dc29f2e6682
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 09:34:42 +0900

    Title: e 제거하기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/e 제거하기/e-to-remove.java
+++ 241031/e 제거하기/e-to-remove.java
@@ -3,14 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 문자열을 입력받습니다.
         String str = sc.next();
-        int length = str.length();
-        for (int i = 0; i < length; i++) {
-            if (str.charAt(i) == 'e') {
+
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+        
+        
+        // 문자열을 순회하며 e가 있는지 탐색하고, 그 때마다 e를 제거합니다.
+        for(int i = 0; i < len; i++)
+            if(str.charAt(i) == 'e') {
+                // 해당 문자열이 e라면 그 원소를 제거하고 break문을 이용해 빠져나옵니다.
                 str = str.substring(0, i) + str.substring(i + 1);
+                len--;
                 break;
             }
-        }
+        
+        // e가 제거된 문자열을 출력합니다.
         System.out.println(str);
     }
 }
