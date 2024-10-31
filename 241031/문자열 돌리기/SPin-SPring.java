ccdc8790f81a1fe304c054f9c690b1e5eca3c8d6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 14:08:50 +0900

    Title: 문자열 돌리기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/문자열 돌리기/SPin-SPring.java
+++ 241031/문자열 돌리기/SPin-SPring.java
@@ -3,12 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 문자열을 입력받습니다.
         String str = sc.next();
-        int L = str.length();
-        for (int i = 0; i <= L; i++) {
-            String str1 = str;
-            str1 = str1.substring(L - i) + str1.substring(0, L - i);
-            System.out.println(str1);
+        
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+        
+        // 문자열을 출력합니다.
+        System.out.println(str);
+        
+        // 문자열을 오른쪽으로 한 칸 쉬프트하고 문자열을 출력합니다. 이를 len회 반복합니다.
+        for(int i = 0; i < len; i++) {
+            // 문자열을 오른쪽으로 한 칸 쉬프트합니다.
+            str = str.substring(len - 1) + str.substring(0, len - 1);
+            
+            // 문자열을 출력합니다.
+            System.out.println(str);
         }
     }
 }
