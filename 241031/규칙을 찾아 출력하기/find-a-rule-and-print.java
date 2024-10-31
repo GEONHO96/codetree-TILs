f91ab3c90a2c2aee8398dc654bc261622e58a0ac
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 20:03:29 +0900

    Title: 규칙을 찾아 출력하기, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241031/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 241031/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -3,21 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
         int n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print("* ");
-                }
-            }
-            for (int j = 0; j < i; j++) {
-                System.out.print("* ");
-            }
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print("  ");
-            }
-            if (i != 0) {
-                System.out.print("* ");
+
+        // n칸의 정사각형에서 i, j가 조건에 맞으면 *을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i > j || i == 0 || j == n - 1) System.out.print("* ");
             }
             System.out.println();
         }
