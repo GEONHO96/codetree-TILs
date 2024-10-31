b5b0a1c0fd7502ab82515097df8d0c22c0464801
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 19:39:10 +0900

    Title: 대문자 소문자 바꾸기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
+++ 241031/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        int length = str.length();
-        for (int i = 0; i < length; i++) {
-            if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
-                System.out.print((char)(str.charAt(i) - 'a' + 'A'));
-            }
-            if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+
+        // 문자열을 정의합니다.
+        String str;
+
+        // 문자열을 입력받습니다.
+        str = sc.next();
+
+        // 문자열의 길이를 구합니다.
+        int len = str.length();
+        
+        // 소문자와 대문자를 바꾸어 출력합니다.
+        for(int i = 0; i < len; i++) {
+            if(str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
                 System.out.print((char)(str.charAt(i) - 'A' + 'a'));
+            }
+            else {
+                System.out.print((char)(str.charAt(i) - 'a' + 'A'));
             }
         }
     }
