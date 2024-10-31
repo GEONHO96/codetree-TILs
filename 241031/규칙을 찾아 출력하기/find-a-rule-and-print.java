e7035b21de5f4acf371dde7bec05bdb5c82d81ae
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 20:03:55 +0900

    Title: 규칙을 찾아 출력하기, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 241031/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -11,6 +11,7 @@ public class Main {
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(i > j || i == 0 || j == n - 1) System.out.print("* ");
+                else System.out.print("  ");
             }
             System.out.println();
         }
