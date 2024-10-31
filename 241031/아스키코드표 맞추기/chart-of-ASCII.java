6944c931a1483ab0be645eaaf26e134c65a709f7
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 17:05:32 +0900

    Title: 아스키코드표 맞추기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/아스키코드표 맞추기/chart-of-ASCII.java
+++ 241031/아스키코드표 맞추기/chart-of-ASCII.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 5; i++) {
-            int num = sc.nextInt();
-            char ch = (char) num;
-            System.out.print(ch + " ");
+
+        // 숫자 5개를 입력받습니다.
+        int[] a = new int[5];
+        for(int i = 0; i < 5; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        // 주어진 값에 해당하는 아스키코드의 문자를 출력합니다.
+        for(int i = 0; i < 5; i++) {
+            System.out.print((char)a[i] + " ");
         }
     }
 }
