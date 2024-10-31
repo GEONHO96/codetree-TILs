5ed02c46bfc1dd8a7487636930be104835b2aa93
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 08:45:55 +0900

    Title: 각 자리 숫자들의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241101/각 자리 숫자들의 합/sum-of-each-digit.java
+++ 241101/각 자리 숫자들의 합/sum-of-each-digit.java
@@ -3,13 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String str = Integer.toString(n);
-        int length = str.length();
-        int sum = 0;
-        for (int i = 0; i < length; i++) {
-            sum += Integer.parseInt(String.valueOf(str.charAt(i)));
-        }
-        System.out.println(sum);
+
+        // 숫자를 입력받습니다.
+        int a = sc.nextInt();
+        String str;
+        int cnt = 0;
+
+        // 숫자를 문자열로 바꿉니다.
+        aStr = Integer.toString(a);
+
+        // 문자열의 길이를 구합니다.
+        int len = aStr.length();
+
+        // 문자열에서 각 자리 숫자들의 합을 구합니다.
+        for(int i = 0; i < len; i++)
+            cnt += aStr.charAt(i) - '0';
+
+        // 각 자리 숫자의 합을 출력합니다.
+        System.out.print(cnt);
     }
 }
