714997bc66f372749ba6281b33152b2c63ccc77c
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-10-31 17:34:27 +0900

    Title: 그 전 알파벳, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241031/그 전 알파벳/before-the-alphabet.java
+++ 241031/그 전 알파벳/before-the-alphabet.java
@@ -7,7 +7,7 @@ public class Main {
         if (ch == 'a') {
             System.out.println("z");
         } else {
-            System.out.println((char)ch - 1);
+            System.out.println((char)(ch - 1));
         }
     }
 }
