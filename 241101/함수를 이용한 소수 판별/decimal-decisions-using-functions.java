71c98d0e4fc4af32ed410201e5f2daaa323df530
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 22:50:44 +0900

    Title: 함수를 이용한 소수 판별, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 241101/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static boolean isPrimeNumber(int n) {
+        if (n == 1) return false;
         for (int i = 2; i < n; i++)
             if (n % i == 0)
                 return false;
