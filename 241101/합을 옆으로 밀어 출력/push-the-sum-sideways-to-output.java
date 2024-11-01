48579d3e47abbab9ee12eabaa3ec58380eb34e5b
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 09:00:31 +0900

    Title: 합을 옆으로 밀어 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241101/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.java
+++ 241101/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // n을 입력받습니다.
         int n = sc.nextInt();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
-            sum += num;
+        int num = 0;
+        String numStr;
+
+        // 숫자를 입력받아 숫자들의 합을 구합니다.
+        for(int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            num += a;
         }
-        String str = Integer.toString(sum);
-        int length = str.length();
-        str = str.substring(1, length) + str.substring(0, 1);
-        System.out.println(str);
+
+        // 더한 값을 문자열로 바꿉니다.
+        numStr = Integer.toString(num);
+
+        // 문자열을 좌측으로 한 칸 밉니다.
+        numStr = numStr.substring(1) + numStr.subString(0, 1);
+
+        // 구하고자 하는 문자열을 출력합니다.
+        System.out.print(numStr);
     }
 }
