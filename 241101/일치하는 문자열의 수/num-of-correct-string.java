550d31e4afbd44e7965174b80abf8e2a89eb1ae8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 09:18:34 +0900

    Title: 일치하는 문자열의 수, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/일치하는 문자열의 수/num-of-correct-string.java
+++ 241101/일치하는 문자열의 수/num-of-correct-string.java
@@ -15,7 +15,7 @@ public class Main {
         // 입력받는 문자열이 str과 같은 것의 개수를 확인합니다.
         for(int i = 0; i < n; i++) {
             // 문자열을 입력받습니다.
-            String str = sc.next();
+            String str2 = sc.next();
 
             // 문자열이 str과 같을 경우 cnt를 1 더해줍니다.
             if(str.equals(str2))
