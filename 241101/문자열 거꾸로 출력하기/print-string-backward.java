a0a8c09def905c2dfada07859ec390b293dc85e9
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-01 12:48:50 +0900

    Title: 문자열 거꾸로 출력하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241101/문자열 거꾸로 출력하기/print-string-backward.java
+++ 241101/문자열 거꾸로 출력하기/print-string-backward.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
+        
+        // 최대 10회 반복합니다.
+        for(int i = 0; i < 10; i++) {
+            // 문자열을 입력받습니다.
             String str = sc.next();
-            int length = str.length();
-            if (str.equals("END")) {
+            
+            // str이 END일 경우 종료합니다.
+            if(str.equals("END"))
                 break;
-            }
-            for (int i = length - 1; i >= 0; i--) {
-                System.out.print(str.charAt(i));
-            }
+            
+            // 문자열의 길이를 구합니다.
+            int len = str.length();
+            
+            // 문자열을 역으로 출력합니다.
+            for(int j = len - 1; j >= 0; j--)
+                System.out.print(str.charAt(j));
             System.out.println();
         }
     }
