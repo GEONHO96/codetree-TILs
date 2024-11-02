ba6cf95fcc30f8aeb52f73f658c03934bf24d335
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-02 22:40:42 +0900

    Title: 두 수의 거듭제곱, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 241102/두 수의 거듭제곱/two-numbers-of-squares.java
+++ 241102/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
-
+    // a^b의 값을 반환합니다.
     public static int power(int a, int b) {
-        int x = 1;
-        for (int i = 1; i <= b; i++) {
-            x *= a;
-        }
-        return x;
+        int cnt = 1;
+        for(int i = 1; i <= b; i++)
+            cnt *= a;
+
+        return cnt;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println(power(a, b));
+
+        System.out.print(power(a, b));
+        
     }
 }
