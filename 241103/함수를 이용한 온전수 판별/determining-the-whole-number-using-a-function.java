e3faa26691edfa56e0c2ac26db3cc121dec56fcd
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-03 00:42:58 +0900

    Title: 함수를 이용한 온전수 판별, Time: 128ms, Memory: 9MB, Status: Passed - CodeTree

--- 241103/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 241103/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -1,24 +1,30 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static boolean isMagicNumber(int n) {
-        if (n % 2 != 0 && n % 10 != 5 && (n % 3 != 0 || n % 9 == 0)) {
+    // 온전수인지 아닌지 여부를 판단하는 함수를 작성합니다.
+    public static boolean judgeNumber(int n) {
+        if(n % 2 == 0)
+            return false;
+        else if(n % 10 == 5)
+            return false;
+        else if(n % 3 == 0 && n % 9 != 0)
+            return false;
+        else
             return true;
-        }
-        return false;
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
         int a = sc.nextInt();
         int b = sc.nextInt();
         int cnt = 0;
-        for (int i = a; i <= b; i++) {
-            if(isMagicNumber(i)) {
+
+        for(int i = a; i <= b; i++) {
+            if(judgeNumber(i))
                 cnt++;
-            }
         }
-        System.out.println(cnt);
+
+        System.out.print(cnt);
     }
 }
