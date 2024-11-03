8de61bb631a9783f7c0f54a7086d96f3dc8b2646
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-03 23:21:30 +0900

    Title: 두 정수 값 교환하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 241103/두 정수 값 교환하기/to-exchange-two-integer-values.java
+++ 241103/두 정수 값 교환하기/to-exchange-two-integer-values.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class IntWrapper {
     int value;
-
+    
     public IntWrapper(int value) {
         this.value = value;
     }
@@ -14,14 +14,24 @@ public class Main {
         n.value = m.value;
         m.value = temp;
     }
-
+    
     public static void main(String[] args) {
+        // 변수 선언 및 입력:
         Scanner sc = new Scanner(System.in);
-        IntWrapper n = new IntWrapper(sc.nextInt());
-        IntWrapper m = new IntWrapper(sc.nextInt());
-
-        swap(n, m);
-
-        System.out.println(n.value + " " + m.value);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        // java의 경우 int값을 그대로 넘겨 swap을 기대하기는 어렵기에,
+        // IntWrapper라는 객체를 만들어 넘기는 방식으로 해결합니다.
+        IntWrapper nWrapper = new IntWrapper(n);
+        IntWrapper mWrapper = new IntWrapper(m);
+        
+        swap(nWrapper, mWrapper);
+        
+        // 교환 이후 값을 다시 넣어줍니다.
+        n = nWrapper.value;
+        m = mWrapper.value;
+        
+        System.out.println(n + " " + m);
     }
 }
