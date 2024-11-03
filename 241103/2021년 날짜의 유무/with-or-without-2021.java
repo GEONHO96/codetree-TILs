c413cd21bfcf8b39b9395250c4be366962cab40d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-03 22:46:32 +0900

    Title: 2021년 날짜의 유무, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241103/2021년 날짜의 유무/with-or-without-2021.java
+++ 241103/2021년 날짜의 유무/with-or-without-2021.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static boolean isYearDate(int M, int D) {
-        if (M == 1 || M == 3 || M == 5 || M == 7 || M == 8 || M == 10 || M == 12) {
-            if (1 <= D && D <= 31) return true;
-        }
-        if (M == 4 || M == 6 || M == 9 || M == 11) {
-            if (1 <= D && D <= 30) return true;
-        }
-        if (M == 2) {
-            if (1 <= D && D <= 28) return true;
-        }
+    // 윤년이 아닐 때 m번째 달의 마지막 날을 반환하는 함수를 작성합니다.
+    public static int lastDayNumber(int m) {
+        if(m == 2)
+            return 28;
+        if(m == 4 || m == 6 || m == 9 || m == 11)
+            return 30;
+        
+        return 31;
+    }
+    
+    // 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+    public static boolean judgeDay(int m, int d) {
+        if(m <= 12 && d <= lastDayNumber(m))
+            return true;
+        
         return false;
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int M = sc.nextInt();
-        int D = sc.nextInt();
-        if (isYearDate(M, D)) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        // 변수 선언 및 입력:
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        
+        if(judgeDay(m, d))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
