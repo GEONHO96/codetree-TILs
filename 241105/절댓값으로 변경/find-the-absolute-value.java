8ba2307b4c9f7a139242a7550eccb90b2e8844fa
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-05 01:19:06 +0900

    Title: 절댓값으로 변경, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241105/절댓값으로 변경/find-the-absolute-value.java
+++ 241105/절댓값으로 변경/find-the-absolute-value.java
@@ -2,28 +2,29 @@ import java.util.Scanner;
 
 public class Main {
     public static final int MAX_N = 50;
-
-    public static int N;
+    
+    public static int n;
     public static int[] arr = new int[MAX_N];
-
+    
+    // 배열의 값들을 그 값의 절대값으로 변경합니다.
+    // call by reference로 구현합니다.
     public static void absoluteValue(int[] arr) {
-        for(int i = 0; i < arr.length; i++) {
-            if (arr[i] < 0) {
+        for(int i = 0; i < n; i++)
+            if(arr[i] < 0)
                 arr[i] = -arr[i];
-            }
-        }
+        
+        return;
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        for (int i = 0; i < N; i++) {
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
             arr[i] = sc.nextInt();
-        }
+        
         absoluteValue(arr);
-
-        for (int i = 0; i < N; i++) {
+        
+        for(int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
-        }
     }
 }
