0891a97a708979e3f0fbff3c1fd432ffb72a4605
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-05 09:52:14 +0900

    Title: 2개 이상의 알파벳, Time: 111ms, Memory: 10MB, Status: Passed - CodeTree

--- 241105/2개 이상의 알파벳/more-than-one-alphabet.java
+++ 241105/2개 이상의 알파벳/more-than-one-alphabet.java
@@ -1,28 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+    // 문자열에 알파벳이 2개 이상인지 여부를 반환합니다.
+    public static boolean isMoreTwoalp(String str) {
+        // 하나라도 서로 다른 알파벳이 있다면
+        // 그 문자열은 알파벳이 2개 이상입니다.
+        int len = str.length();
+        for(int i = 0; i < len; i++)
+            if(str.charAt(i) != str.charAt(0))
+                return true;
 
-    public static int alphabets(String str) {
-        char[] ch = new char[100];
-        int cnt = 1;
-        for (int i = 0; i < str.length(); i++) {
-            ch[i] = str.charAt(i);
-        }
-        for (int i = 0; i < str.length(); i++) {
-            if (ch[i] != ch[i + 1]) {
-                cnt++;
-            }
-        }
-        return cnt;
+        return false;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        if (alphabets(str) > 2) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        // 변수 선언 및 입력:
+        String A = sc.next();
+
+        if(isMoreTwoalp(A))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
