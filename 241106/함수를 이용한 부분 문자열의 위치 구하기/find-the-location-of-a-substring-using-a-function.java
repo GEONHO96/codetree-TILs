271936c3d01ffe2723cef2805cf7c89dab333d0f
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-06 00:56:47 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241106/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static boolean isPartialString(String inputString, String objectString) {
+        int N = inputString.length();
+        for (int i = 0; i < N; i++) {
+            if (objectString.charAt(0) == inputString.charAt(i)) {
+                if (inputString.substring(i, N).equals(objectString)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static int partialString(String inputString, String objectString) {
+        int N = inputString.length();
+        int k = 0;
+        for (int i = 0 ; i < N; i++) {
+            if (objectString.charAt(0) == inputString.charAt(i)) {
+                if (inputString.substring(i, N).equals(objectString)) {
+                    k = i;
+                }
+            }
+        }
+        return k;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String inputString = sc.next();
+        String objectString = sc.next();
+        if (isPartialString(inputString, objectString)) {
+            System.out.println(partialString(inputString, objectString));
+        } else {
+            System.out.println(-1);
+        }
+    }
+}
