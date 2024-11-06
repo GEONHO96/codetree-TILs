92d72472f2e09654c933db7fb2e5b85f681d937e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-06 10:12:48 +0900

    Title: 반복 출력하기 2, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

--- 241106/반복 출력하기 2/repeated-output-2.java
+++ 241106/반복 출력하기 2/repeated-output-2.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void printString(int N) {
-        if(N == 0)
-            return;
-
-        printString(N - 1);
+    public static void printString(int n) { // 1부터 n번째 줄까지 문자열을 출력하는 함수
+        if(n == 0)                          // n이 0이라면, 더 이상 진행하지 않고
+            return;                         // 퇴각합니다.
+        
+        printString(n - 1);
         System.out.println("HelloWorld");
     }
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        printString(N);
+        
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+        printString(n);
     }
 }
