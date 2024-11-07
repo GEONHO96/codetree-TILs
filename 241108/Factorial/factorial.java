5fc1bae826a477bc0ad663c9adfc2cdd50efc1a0
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-08 08:57:45 +0900

    Title: Factorial, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241108/Factorial/factorial.java
+++ 241108/Factorial/factorial.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int factorial(int N) {
-        if (N == 0)
+    public static void factorial(int n) {
+        if(n == 0)
             return 1;
-        return N * factorial(N - 1);
+
+        return factorial(n - 1) * n;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        System.out.println(factorial(N));
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+
+        System.out.print(factorial(n));
     }
 }
