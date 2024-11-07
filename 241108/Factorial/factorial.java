5b2a6c37205346fc0e3ed962717818b2696eb239
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-08 08:42:40 +0900

    Title: Factorial, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241108/Factorial/factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int factorial(int N) {
+        if (N == 0)
+            return 1;
+        return N * factorial(N - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(factorial(N));
+    }
+}
