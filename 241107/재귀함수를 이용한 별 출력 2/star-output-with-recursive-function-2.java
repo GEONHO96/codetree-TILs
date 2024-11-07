2ccb1a7b50e50f37f7943f73e394710b755cbbc8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-07 12:05:01 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241107/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
+++ 241107/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.java
@@ -1,33 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printStarReverse(int n) {
-        if (n == 0)
-            return;
+    public static void printStar(int n) { // 1부터 2n번째 줄까지 주어진 모양대로 별을 출력하는 함수
+        if(n == 0)            // n이 0이라면, 더 이상 진행하지 않고
+            return;           // 퇴각합니다.
 
-        for (int i = 0; i < n; i++) {
+        // 외각 별을 출력합니다.
+        for(int i = 1; i <= n; i++)
             System.out.print("* ");
-        }
         System.out.println();
-        printStarReverse(n - 1);
-    }
 
-    public static void printStar(int n) {
-        if (n == 0)
-            return;
+        printStar(n - 1); // 가운데 별을 출력하는 함수
 
-        printStar(n - 1);
-        for (int i = 0; i < n; i++) {
+        // 외각 별을 출력합니다.
+        for(int i = 1; i <= n; i++)
             System.out.print("* ");
-        }
         System.out.println();
     }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
-        printStarReverse(n);
+
         printStar(n);
     }
 }
