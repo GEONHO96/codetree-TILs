0f6fa46d683d8658b86595665bcdea21b83baa6d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-07 11:44:55 +0900

    Title: 재귀함수의 꽃, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241107/재귀함수의 꽃/the-flower-of-the-recursive-function.java
+++ 241107/재귀함수의 꽃/the-flower-of-the-recursive-function.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void printNumber(int N) {
-        if (N == 0)
+    // 재귀함수를 이용해 숫자를 n부터 1까지 출력하고, 다시 1부터 n까지 출력합니다.
+    public static void printStar(int n) {
+        if(n == 0)
             return;
 
-        printNumber(N - 1);
-        System.out.print(N + " ");
-    }
-
-    public static void printReverseNumber(int N) {
-        if(N == 0)
-            return;
-
-        System.out.print(N + " ");
-        printReverseNumber(N - 1);
+        System.out.print(n + " ");
+        printStar(n - 1);
+        System.out.print(n + " ");
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        printReverseNumber(N);
-        printNumber(N);
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+
+        printStar(n);
     }
 }
