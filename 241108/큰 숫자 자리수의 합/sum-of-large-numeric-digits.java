27b2cb4aa0b5a052276b67b180adf6ebeaf826e9
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-08 12:48:34 +0900

    Title: 큰 숫자 자리수의 합, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

--- 241108/큰 숫자 자리수의 합/sum-of-large-numeric-digits.java
+++ 241108/큰 숫자 자리수의 합/sum-of-large-numeric-digits.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-
+    // n의 각 자릿수의 합을 반환합니다.
     public static int digitSum(int n) {
-        if (n == 0)
-            return 0;
+        if(n < 10)
+            return n;
+
         return digitSum(n / 10) + n % 10;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-        int D = A * B * C;
-        System.out.println(digitSum(D));
+        // 변수 선언 및 입력:
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.print(digitSum(a * b * c));
     }
 }
