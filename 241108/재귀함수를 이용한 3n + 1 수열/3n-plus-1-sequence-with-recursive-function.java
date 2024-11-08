6b8196aa6495fbaf3bcd018f5c88b1f08a04e392
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-08 14:23:28 +0900

    Title: 재귀함수를 이용한 3n + 1 수열, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241108/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
+++ 241108/재귀함수를 이용한 3n + 1 수열/3n-plus-1-sequence-with-recursive-function.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    // a가 3n + 1 수열을 총 몇번 반복해야 1이 되는지 반환합니다.
+    public static int countNumber(int a) {
+        if(a == 1)
+            return 0;
 
-    public static int recursion(int n) {
-        int cnt = 0;
-        while (n > 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-                cnt++;
-            } else {
-                n = 3 * n + 1;
-                cnt++;
-            }
-        }
-        return cnt;
+        if(a % 2 == 0)
+            return countNumber(a / 2) + 1;
+        else
+            return countNumber(3 * a + 1) + 1;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
-        System.out.println(recursion(n));
+
+        System.out.print(countNumber(n));
     }
 }
