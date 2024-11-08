57839295713cd36d6ea46623429f7f031f1604b8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-09 00:04:38 +0900

    Title: 단어 정렬, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 241109/단어 정렬/sorting-words.java
+++ 241109/단어 정렬/sorting-words.java
@@ -4,15 +4,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String[] str = new String[n];
-        for (int i = 0; i < n; i++) {
-            str[i] = sc.next();
-        }
-        Arrays.sort(str);
 
-        for (int i = 0; i < n; i++) {
-            System.out.println(str[i]);
-        }
+        // 입력
+        int n = sc.nextInt();
+        String[] wordList = new String[n];
+        for(int i = 0; i < n; i++)
+            wordList[i] = sc.next();
+
+        // 문자열 사전순 정렬
+        Arrays.sort(wordList);
+
+        for(int i = 0; i < n; i++)
+            System.out.println(wordList[i]);
     }
 }
