75b38542ae0c298c6aef20327592a2fba20ddb0e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-08 22:35:28 +0900

    Title: 오름 내림차순 정렬, Time: 307ms, Memory: 15MB, Status: Passed - CodeTree

--- 241108/오름 내림차순 정렬/inc-dec-sorting.java
+++ 241108/오름 내림차순 정렬/inc-dec-sorting.java
@@ -5,20 +5,26 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
+        int[] nums = new int[n];
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        // 오름차순 정렬
+        Arrays.sort(nums);
+
+        for(int i = 0; i < n; i++)
+            System.out.print(nums[i] + " ");
         System.out.println();
-        Integer[] arr1 = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-        Arrays.sort(arr1, Collections.reverseOrder());
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr1[i] + " ");
-        }
+
+        // 내림차순 정렬
+        Integer[] nums2 = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Arrays.sort(nums2, Collections.reverseOrder());
+
+        for(int i = 0; i < n; i++)
+            System.out.print(nums2[i] + " ");
+        System.out.println();
     }
 }
