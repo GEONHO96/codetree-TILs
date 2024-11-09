910b7b1b0ba84520945cc87d3c400c065edf7bb1
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-09 23:09:09 +0900

    Title: 007, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241109/007/007.java
+++ 241109/007/007.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
 
-class Program {
-    String code;
-    char point;
+// Spy 정보를 나타내는 클래스 선언
+class Spy {
+    String secretCode;
+    char meetingPoint;
     int time;
 
-    public Program(String code, char point, int time) {
-        this.code = code;
-        this.point = point;
+    public Spy(String secretCode, char meetingPoint, int time) {
+        this.secretCode = secretCode;
+        this.meetingPoint = meetingPoint;
         this.time = time;
     }
-}
+};
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String code = sc.next();
-        char point = sc.next().charAt(0);
-        int time = sc.nextInt();
-        Program program = new Program(code, point, time);
-        System.out.println("secret code : " + program.code);
-        System.out.println("meeting point : " + program.point);
-        System.out.println("time : " + program.time);
+        
+        // 변수 선언 및 입력
+        String sCode;
+        char mPoint;
+        int time;
+        
+        sCode = sc.next();
+        mPoint = sc.next().charAt(0);
+        time = sc.nextInt();
+        
+        // Spy 객체를 만들어 줍니다.
+        Spy sp = new Spy(sCode, mPoint, time);
+        
+        // 결과를 출력합니다.
+        System.out.println("secret code : " + sp.secretCode);
+        System.out.println("meeting point : " + sp.meetingPoint);
+        System.out.println("time : " + sp.time);
     }
 }
