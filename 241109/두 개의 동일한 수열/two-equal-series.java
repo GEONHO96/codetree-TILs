c1821287eace94c4e1170010f59b09251a34a8d6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-09 15:15:18 +0900

    Title: 두 개의 동일한 수열, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 241109/두 개의 동일한 수열/two-equal-series.java
+++ 241109/두 개의 동일한 수열/two-equal-series.java
@@ -2,29 +2,41 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Main {
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] a = new int[MAX_N];
+    public static int[] b = new int[MAX_N];
+
+    public static boolean equal() {
+        // n개의 원소를 순서대로 봤을 때
+        // 전부 동일한 경우에만 일치합니다.
+        // 단 하나라도 다르다면, false입니다.
+        for(int i = 0; i < n; i++)
+            if(a[i] != b[i])
+                return false;
+
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] A = new int[n];
-        for(int i = 0; i < n; i++) {
-            A[i] = sc.nextInt();
-        }
-        Arrays.sort(A);
-        int[] B = new int[n];
-        for(int i = 0; i < n; i++) {
-            B[i] = sc.nextInt();
-        }
-        Arrays.sort(B);
-        boolean satisfied = true;
-        for(int i = 0; i < n; i++) {
-            if (A[i] != B[i]) {
-                satisfied = false;
-            }
-        }
-        if(satisfied == true) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            b[i] = sc.nextInt();
+
+        // 정렬
+        Arrays.sort(a, 0, n);
+        Arrays.sort(b, 0, n);
+
+        // 수열이 일치하는지 확인합니다.
+        if(equal())
+            System.out.print("Yes");
+        else
+            System.out.print("No");
     }
 }
