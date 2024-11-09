75bb5f8599960c5f039899586dcfbb1550a66846
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-10 01:35:50 +0900

    Title: Next Level, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 241110/Next Level/next-level.java
+++ 241110/Next Level/next-level.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
 
 class User {
-    String userId;
+    String id;
     int level;
 
-    public User(String userId, int level) {
-        this.userId = userId;
+    public User() {
+        this.id = "";
+        this.level = 0;
+    }
+
+    public User(String id, int level) {
+        this.id = id;
         this.level = level;
     }
 };
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] ars) {
         Scanner sc = new Scanner(System.in);
-        String userId = sc.next();
-        int level = sc.nextInt();
-        User user1 = new User(userId, level);
-        User user2 = new User("codetree", 10);
-        System.out.println("user " + user2.userId + " lv " + user2.level);
-        System.out.println("user " + user1.userId + " lv " + user1.level);
+
+        // 초깃값을 이용하여 첫 번째 User 객체를 만들어 줍니다.
+        User user1 = new User();
+
+        // 값을 변경합니다.
+        user1.id = "codetree";
+        user1.level = 10;
+
+        // 변수 선언 및 입력
+        String id2 = sc.next();
+        int level2 = sc.nextInt();
+
+        // 초기값을 이용하여 두 번째 User 객체를 만들어 줍니다.
+        User user2 = new User();
+
+        // 값을 변경합니다.
+        user2.id = id2;
+        user2.level = level2;
+
+        // 결과를 출력합니다.
+        System.out.println("user " + user1.id + " lv " + user1.level);
+        System.out.println("user " + user2.id + " lv " + user2.level);
     }
 }
