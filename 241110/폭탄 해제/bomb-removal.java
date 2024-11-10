b226e47e887f1ff441fc2d01d0584142a1017367
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-10 23:14:06 +0900

    Title: 폭탄 해제, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 241110/폭탄 해제/bomb-removal.java
+++ 241110/폭탄 해제/bomb-removal.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 
-class Bomb {
-    String code;
-    char color;
-    int second;
-    public Bomb(String code, char color, int second) {
-        this.code = code;
-        this.color = color;
-        this.second = second;
+// Bomb 정보를 나타내는 클래스 선언
+class bomb {
+    String unlockCode;
+    char linearColor;
+    int time;
+
+    public Bomb(String unlockCode, char linearColor, int time) {
+        this.unlockCode = unlockCode;
+        this.linearColor = linearColor;
+        this.time = time;
     }
-}
+};
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String code = sc.next();
-        char color = sc.next().charAt(0);
-        int second = sc.nextInt();
-        Bomb bomb = new Bomb(code, color, second);
-        System.out.println("code : " + bomb.code);
-        System.out.println("color : " + bomb.color);
-        System.out.println("second : " + bomb.second);
+        // 변수 선언 및 입력
+        String uCode = sc.next();
+        char lColor = sc.next().charAt(0);
+        int time = sc.nextInt();
+
+        // Bomb 객체를 만들어 줍니다.
+        Bomb b = new Bomb(uCode, lColor, time);
+
+        // 결과를 출력합니다.
+        System.out.println("code : " + b.unlockCode);
+        System.out.println("color : " + b.linearColor);
+        System.out.println("second : " + b.time);
     }
 }
