c2114c7d13358ea91520cda67fa4b1afa8dbb1d6
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-10 22:11:57 +0900

    Title: 코드네임, Time: 130ms, Memory: 9MB, Status: Passed - CodeTree

--- 241110/코드네임/code-name.java
+++ 241110/코드네임/code-name.java
@@ -1,10 +1,11 @@
 import java.util.Scanner;
 
-class Agent {
+// User 정보를 나타내는 클래스 선언
+class User {
     char codeName;
     int score;
 
-    public Agent(char codeName, int score) {
+    public User(char codeName, int score){
         this.codeName = codeName;
         this.score = score;
     }
@@ -12,22 +13,27 @@ class Agent {
 
 public class Main {
     public static void main(String[] args) {
-        int min = 100;
         Scanner sc = new Scanner(System.in);
-        Agent[] agents = new Agent[5];
-        for (int i = 0; i < 5; i++) {
+        
+        // 변수 선언 및 입력
+        int n = 5;
+        User[] users=  new User[n];
+        for(int i = 0; i < n; i++) {
             char codeName = sc.next().charAt(0);
             int score = sc.nextInt();
-            agents[i] = new Agent(codeName, score);
-            if (min > score) {
-                min = score;
-            }
+            
+            // 객체 생성 및 리스트에 추가
+            users[i] = new User(codeName, score);
         }
-        for (int i = 0; i < 5; i++) {
-            if (agents[i].score == min) {
-                System.out.print(agents[i].codeName + " ");
-                System.out.print(agents[i].score);
-            }
+        
+        // 최소 점수를 갖는 요원 찾기
+        int minIdx = 0;
+        for(int i = 1; i < n; i++) {
+            if(users[minIdx].score > users[i].score)
+                minIdx = i;
         }
+        
+        // 결과를 출력합니다.
+        System.out.println(users[minIdx].codeName + " " + users[minIdx].score);
     }
 }
