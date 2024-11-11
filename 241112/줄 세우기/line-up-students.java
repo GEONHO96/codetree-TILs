a177ada9f5937bf434acef149cc6c485c96deff1
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-12 07:55:53 +0900

    Title: 줄 세우기, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 241112/줄 세우기/line-up-students.java
+++ 241112/줄 세우기/line-up-students.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+// 학생들의 정보를 나타내는 클래스 선언
 class Student implements Comparable<Student> {
-    int height;
-    int weight;
-    int number;
+    int height, weight, number;
+
     public Student(int height, int weight, int number) {
         this.height = height;
         this.weight = weight;
@@ -12,27 +12,41 @@ class Student implements Comparable<Student> {
     }
 
     @Override
-    public int compareTo(Student students) {
-        if (this.height == students.height)
-            return students.weight - this.weight;
-        return students.height - this.height;
+    public int compareTo(Student student) {
+        // 키가 크면 정렬 했을 때 앞에 와야 합니다.
+        if(this.height != student.height)
+            return student.height - this.height;
+        // 몸무게가 크면 정렬 했을 때 앞에 와야합니다.
+        if(this.weight != student.weight)
+            return student.weight - this.weight;
+        // 번호가 작으면 앞에 와야 합니다.
+        return this.number - student.number;
     }
 };
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Student[] students = new Student[N];
-        for (int i = 0; i < N; i++) {
+
+        // 변수 선언 및 입력:
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+        for(int i = 0; i < n; i++) {
             int height = sc.nextInt();
             int weight = sc.nextInt();
-            int number = i + 1;
-            students[i] = new Student(height, weight, number);
+
+            // Student 객체를 생성해 리스트에 추가합니다.
+            students[i] = new Student(height, weight, i + 1);
         }
 
+        // custom comparator 활용한 정렬
         Arrays.sort(students);
-        for (int i = 0; i < N; i++)
-            System.out.println(students[i].height + " " + students[i].weight + " " + students[i].number);
+
+        // 결과를 출력합니다.
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].height + " ");
+            System.out.print(students[i].weight + " ");
+            System.out.println(students[i].number);
+        }
     }
 }
