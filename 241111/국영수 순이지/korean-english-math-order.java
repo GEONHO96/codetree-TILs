2e29114f823e5b802b9dfe6de3112acc5dd6bce2
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-11 15:39:56 +0900

    Title: 국영수 순이지, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/국영수 순이지/korean-english-math-order.java
+++ 241111/국영수 순이지/korean-english-math-order.java
@@ -1,46 +1,57 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+// 학생들의 정보를 나타내는 클래스 선언
 class Student implements Comparable<Student> {
     String name;
-    int kor, eng, math;
+    int korean, english, math;
 
-    public Student(String name, int kor, int eng, int math) {
+    public Student(String name, int korean, int english, int math){
         this.name = name;
-        this.kor = kor;
-        this.eng = eng;
+        this.korean = korean;
+        this.english = english;
         this.math = math;
     }
 
     @Override
-    public int compareTo(Student students) {
-        if (this.kor == students.kor) {
-            if (this.eng == students.eng) {
-                return students.math - this.math;
-            }
-            return students.eng - this.eng;
-        }
-        return students.kor - this.kor;
+    public int compareTo(Student student) {
+        // 국어 점수가 높으면 정렬 했을 때 앞에 와야 합니다.
+        if(this.korean != student.korean)
+            return student.korean - this.korean;
+        // 영어 점수가 높으면 정렬 했을 때 앞에 와야 합니다.
+        if(this.english != student.english)
+            return student.english - this.english;
+        // 수학 점수가 높으면 정렬 했을 때 앞에 와야 합니다.
+        return student.math - this.math;
     }
-}
+};
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
         Student[] students = new Student[n];
         for(int i = 0; i < n; i++) {
             String name = sc.next();
-            int kor = sc.nextInt();
-            int eng = sc.nextInt();
+            int korean = sc.nextInt();
+            int english = sc.nextInt();
             int math = sc.nextInt();
-            students[i] = new Student(name, kor, eng, math);
+
+            // Student 객체를 생성해 리스트에 추가합니다.
+            students[i] = new Student(name, korean, english, math);
         }
 
+        // custom comparator를 활용한 정렬
         Arrays.sort(students);
 
-        for(int i = 0; i < n; i++)
-            System.out.println(students[i].name + " " + students[i].kor + " " + students[i].eng + " " + students[i].math);
-
+        // 결과를 출력합니다.
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].korean + " ");
+            System.out.print(students[i].english + " ");
+            System.out.println(students[i].math);
+        }
     }
 }
