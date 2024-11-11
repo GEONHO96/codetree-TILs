cef598a3e8a721e91cfcd0e2f9f61b9d05ea64d7
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-11 16:28:37 +0900

    Title: 총점 비교, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 241111/총점 비교/compare-total-points.java
+++ 241111/총점 비교/compare-total-points.java
@@ -1,39 +1,52 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+// 학생들의 정보를 나타내는 클래스 선언
 class Student implements Comparable<Student> {
     String name;
-    int subject1, subject2, subject3;
-
-    public Student(String name, int subject1, int subject2, int subject3) {
+    int korean, english, math;
+    
+    public Student(String name, int korean, int english, int math){
         this.name = name;
-        this.subject1 = subject1;
-        this.subject2 = subject2;
-        this.subject3 = subject3;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
     }
-
+    
     @Override
-    public int compareTo(Student students) {
-        return (this.subject1 + this.subject2 + this.subject3) - (students.subject1 + students.subject2 + students.subject3);
+    public int compareTo(Student student) {
+        // 세 점수의 합이 오름차순이 되도록 정렬합니다.
+        return (this.korean + this.english + this.math) -
+               (student.korean + student.english + student.math); 
     }
-}
+};
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
+        // 변수 선언 및 입력:
         int n = sc.nextInt();
         Student[] students = new Student[n];
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             String name = sc.next();
-            int subject1 = sc.nextInt();
-            int subject2 = sc.nextInt();
-            int subject3 = sc.nextInt();
-            students[i] = new Student(name, subject1, subject2, subject3);
+            int korean = sc.nextInt();
+            int english = sc.nextInt();
+            int math = sc.nextInt();
+            
+            // Student 객체를 생성해 리스트에 추가합니다.
+            students[i] = new Student(name, korean, english, math);
         }
-
+        
+        // custom comparator를 활용한 정렬
         Arrays.sort(students);
-
-        for (int i = 0; i < n; i++)
-            System.out.println(students[i].name + " " + students[i].subject1 + " " + students[i].subject2 + " " + students[i].subject3);
+        
+        // 결과를 출력합니다.
+        for (int i = 0; i < n; i++){
+            System.out.print(students[i].name + " ");
+            System.out.print(students[i].korean + " ");
+            System.out.print(students[i].english + " ");
+            System.out.println(students[i].math);
+        }
     }
 }
