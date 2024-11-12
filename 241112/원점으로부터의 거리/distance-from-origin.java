9f9614117d49983e2ba7ad8fb9066352ae2d657e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-12 17:38:24 +0900

    Title: 원점으로부터의 거리, Time: 251ms, Memory: 14MB, Status: Passed - CodeTree

--- 241112/원점으로부터의 거리/distance-from-origin.java
+++ 241112/원점으로부터의 거리/distance-from-origin.java
@@ -1,37 +1,51 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class Coordinate implements Comparable<Coordinate> {
-    int x;
-    int y;
-    int manhattan;
-    int number;
-    public Coordinate(int x, int y, int manhattan, int number) {
-        this.x = x;
-        this.y = y;
-        this.manhattan = manhattan;
-        this.number = number;
+class Point implements Comparable<Point> {
+    int dist;
+    int index;
+    
+    public Point(int dist, int index){
+        this.dist = dist;
+        this.index = index;
     }
+    
     @Override
-    public int compareTo(Coordinate coordinate) {
-        return this.manhattan - coordinate.manhattan;
+    public int compareTo(Point point) {
+        // dist가 작으면 정렬 했을 때 앞에 와야 합니다.
+        if(dist != point.dist)
+            return dist - point.dist;
+        // index가 작으면 정렬 했을 때 앞에 와야 합니다.
+        return index - point.index;
     }
 }
 
 public class Main {
+    public static final int MAXN = 1000;
+    
+    // 원점과의 거리를 계산하는 함수입니다.
+    public static int getDistFromOrigin(int x, int y) {
+        return Math.abs(x) + Math.abs(y);
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Coordinate[] coordinate = new Coordinate[N];
-        for (int i = 0; i < N; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int manhattan = Math.abs(x) + Math.abs(y);
-            int number = i + 1;
-            coordinate[i] = new Coordinate(x, y, manhattan, number);
+        int n = sc.nextInt();
+        
+        Point[] point = new Point[MAXN];
+        
+        int x, y;
+        for(int i = 0; i < n; i++) {
+            x = sc.nextInt();
+            y = sc.nextInt();
+            
+            // 원점과의 거리와 index를 저장합니다.
+            point[i] = new Point(getDistFromOrigin(x, y), i + 1);
         }
-        Arrays.sort(coordinate);
-        for (int i = 0; i < N; i++)
-            System.out.println(coordinate[i].number);
+        
+        Arrays.sort(point, 0, n);
+        
+        for(int i = 0; i < n; i++)
+            System.out.println(point[i].index);
     }
 }
