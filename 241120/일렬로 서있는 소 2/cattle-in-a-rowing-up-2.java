209646b7c59fa47a648c10246613c7aa20bf2829
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-20 07:20:49 +0900

    Title: 일렬로 서있는 소 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
+++ 241120/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] A = new int[N + 1];
+
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        // 모든 쌍을 다 잡아봅니다.
         int cnt = 0;
-        for (int i = 1; i <= N; i++) {
-            A[i] = sc.nextInt();
-        }
-        for (int i = 1; i <= N; i++) {
-            for (int j = i + 1; j <= N; j++) {
-                for (int k = j + 1; k <= N; k++) {
-                    if (A[i] <= A[j] && A[j] <= A[k]) {
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                for(int k = j + 1; k < n; k++)
+                    if(arr[i] <= arr[j] && arr[j] <= arr[k])
                         cnt++;
-                    }
-                }
-            }
-        }
+
         System.out.println(cnt);
     }
 }
