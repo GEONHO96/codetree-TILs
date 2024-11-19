e0f994b5bc8bd5c563a204f2e20c4c526ee267c0
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-20 07:19:38 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/괄호 쌍 만들어주기 3/pair-parentheses-3.java
+++ 241120/괄호 쌍 만들어주기 3/pair-parentheses-3.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String str;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String str = sc.next();
+
+        // 입력
+        str = sc.next();
+        int n = str.length();
+
+        // 모든 쌍을 다 잡아봅니다.
         int cnt = 0;
-        for (int i = 0; i < str.length() - 1; i++) {
-            for (int j = i + 1; j < str.length(); j++) {
-                if (str.charAt(j) == ')' && str.charAt(i) == '(') {
+        for(int i = 0; i < n; i++)
+            for(int j = i + 1; j < n; j++)
+                if(str.charAt(i) == '(' && str.charAt(j) == ')')
                     cnt++;
-                }
-            }
-        }
+
         System.out.println(cnt);
     }
 }
