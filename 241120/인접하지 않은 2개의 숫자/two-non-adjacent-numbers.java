fee5df2c73b3d01907e2e3461b5483c24bf78050
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-20 11:26:58 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
+++ 241120/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int INT_MIN = Integer.MIN_VALUE;
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] a = new int[MAX_N];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        int sumMax = 0;
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] != arr[i + 1]) {
-                    sumMax = Math.max(arr[i] + arr[j], sumMax);
-                }
-            }
-        }
-        System.out.println(sumMax);
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        // 인접하지 않은 모든 쌍을 다 잡아봅니다.
+        int ans = INT_MIN;
+        for(int i = 0; i < n; i++)
+            for(int j = i + 2; j < n; j++)
+                if(ans < a[i] + a[j])
+                    ans = a[i] + a[j];
+
+        System.out.print(ans);
     }
 }
