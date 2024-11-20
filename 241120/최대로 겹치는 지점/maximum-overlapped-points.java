2e52f53c6d3058b5ad1ba2952da3fe5605c2223e
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-20 18:04:47 +0900

    Title: 최대로 겹치는 지점, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

--- 241120/최대로 겹치는 지점/maximum-overlapped-points.java
+++ 241120/최대로 겹치는 지점/maximum-overlapped-points.java
@@ -1,19 +1,36 @@
 import java.util.Scanner;
 
 public class Main {
+    public static final int MAX_X = 100;
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] a = new int[MAX_N + 1];
+    public static int[] b = new int[MAX_N + 1];
+
+    public static int[] blocks = new int[MAX_X + 1];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[101];
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            int x1 = sc.nextInt();
-            int x2 = sc.nextInt();
-            for (int j = x1; j <= x2; j++) {
-                arr[j]++;
-                max = Math.max(max, arr[j]);
-            }
+        // 입력
+        n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++) {
+            a[i] = sc.nextInt();
+            b[i] = sc.nextInt();
         }
-        System.out.println(max);
+
+        // 블럭을 특정 구간에 쌓아줍니다.
+        for(int i = 1; i <= n; i++)
+            for(int j = a[i]; j <= b[i]; j++)
+                blocks[j]++;
+
+        // 최댓값을 구합니다.
+        int max = 0;
+        for(int i = 1; i <= 100; i++)
+            if(blocks[i] > max)
+                max = blocks[i];
+
+        System.out.print(max);
     }
 }
