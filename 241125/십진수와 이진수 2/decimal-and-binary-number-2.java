5b144b29f303c86a6c456c59e3f63b20197152e9
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-25 23:54:12 +0900

    Title: 십진수와 이진수 2, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241125/십진수와 이진수 2/decimal-and-binary-number-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = 0;
+        for (int i = 0; i < str.length(); i++) {
+            n = n * 2 + (str.charAt(i) - '0');
+        }
+        n *= 17;
+        int[] digits=  new int[20];
+        int cnt = 0;
+        while(true) {
+            if (n < 2) {
+                digits[cnt++] = n;
+                break;
+            }
+            digits[cnt++] = n % 2;
+            n /= 2;
+        }
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+    }
+}
