18a39c028f4eebc205c9464199c2183c73e6f4c8
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-11-26 23:54:30 +0900

    Title: 여러가지 진수변환, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241126/여러가지 진수변환/various-numeral-system-transformations.java
+++ 241126/여러가지 진수변환/various-numeral-system-transformations.java
@@ -4,24 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
+        // 입력받기
         int N = sc.nextInt();
         int B = sc.nextInt();
         
+        // 진법 변환 결과를 저장할 문자열
         StringBuilder sb = new StringBuilder();
         
+        // N을 B진법으로 변환
         int temp = N;
         while (temp > 0) {
             int remainder = temp % B;
-            if (remainder > 10) {
-                sb.append((char) (remainder + '0'));
+            if (remainder < 10) {
+                sb.append((char) (remainder + '0')); // 숫자 '0' ~ '9'
             } else {
-                sb.append((char) (remainder - 10 + 'A'));
+                sb.append((char) (remainder - 10 + 'A')); // 문자 'A' ~ 'Z'
             }
             temp /= B;
         }
         
+        // 진법 변환 결과 뒤집기
         String converted = sb.reverse().toString();
         
+        // B진법 수를 다시 10진법으로 변환
         int restored = 0;
         for (int i = 0; i < converted.length(); i++) {
             char c = converted.charAt(i);
@@ -34,6 +39,7 @@ public class Main {
             restored = restored * B + value;
         }
         
+        // 출력
         System.out.println(restored);
     }
 }
