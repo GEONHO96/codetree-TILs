1a2028ac7fd7b5f3fe60ba133cb65e2d1b2541c7
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2024-12-06 14:45:42 +0900

    Title: 색종이의 총 넓이, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 241206/색종이의 총 넓이/the-total-area-of-colored-paper.java
+++ 241206/색종이의 총 넓이/the-total-area-of-colored-paper.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        boolean[][] visited = new boolean[101][101];
+        boolean[][] visited = new boolean[201][201];
         int area = 0;
+        int offset = 100;
         for (int i = 0; i < N; i++) {
             int x = sc.nextInt();
             int y = sc.nextInt();
-            for (int j = x; j < x + 8; j++) {
-                for (int k = y; k < y + 8; k++) {
+            for (int j = x + offset; j < x + 8 + offset; j++) {
+                for (int k = y + offset; k < y + 8 + offset; k++) {
                     if (!visited[j][k]) {
                         visited[j][k] = true;
                         area++;
