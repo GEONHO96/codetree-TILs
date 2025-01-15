12fe6dd69751558757bac7ff929709ae28d0f57d
GEONHO96 <117559957+GEONHO96@users.noreply.github.com>
2025-01-15 15:59:29 +0900

    Title: 정수 명령 처리, Time: 362ms, Memory: 17MB, Status: Passed - CodeTree

--- 250115/정수 명령 처리/process-numeric-commands.java
+++ 250115/정수 명령 처리/process-numeric-commands.java
@@ -4,26 +4,34 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < N; i++) {
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // 스택 선언
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
             String command = sc.next();
-            if (command.equals("push")) {
-                int value = sc.nextInt();
-                stack.push(value);
-            } else if (command.equals("pop")) {
-                System.out.println(stack.peek());
-                stack.pop();
-            } else if (command.equals("size")) {
-                System.out.println(stack.size());
-            } else if (command.equals("empty")) {
-                if (stack.isEmpty()) {
+
+            if(command.equals("push")) {
+                int x = sc.nextInt();
+                s.push(x);
+            }
+            else if(command.equals("pop")) {
+                System.out.println(s.pop());
+            }
+            else if(command.equals("size")) {
+                System.out.println(s.size());
+            }
+            else if(command.equals("empty")) {
+                if(s.isEmpty())
                     System.out.println(1);
-                } else {
+                else
                     System.out.println(0);
-                }
-            } else if (command.equals("top")) {
-                System.out.println(stack.peek());
+            }
+            else {
+                System.out.println(s.peek());
             }
         }
     }
