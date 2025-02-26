19e762cd475f708df59672c0e96d9d285d8e9b1d
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-02-26 15:52:08 +0900

    Title: 변수 값 복사하기 3, Time: 81ms, Memory: 9MB, Status: Passed - CodeTree

--- 250226/변수 값 복사하기 3/copying-variable-values-3.java
+++ 250226/변수 값 복사하기 3/copying-variable-values-3.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1, b = 2, c = 3;
+        // 변수 선언
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        // 값 변경
         a = c;
         b = c;
+
+        // 출력
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
