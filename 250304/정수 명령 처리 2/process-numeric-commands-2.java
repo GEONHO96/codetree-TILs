852226f5e545e1a3d6dc50c8e2a096300f216816
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-04 11:35:47 +0900

    Title: 정수 명령 처리 2, Time: 386ms, Memory: 18MB, Status: Passed - CodeTree

--- 250304/정수 명령 처리 2/process-numeric-commands-2.java
+++ 250304/정수 명령 처리 2/process-numeric-commands-2.java
@@ -1,40 +1,39 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 import java.util.Queue;
 import java.util.LinkedList;
-import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            if (command.equals("push")) {
-                int K = Integer.parseInt(st.nextToken());
-                queue.add(K);
-            } else if (command.equals("pop")) {
-                bw.write(queue.poll() + "\n");
-            } else if (command.equals("size")) {
-                bw.write(queue.size() + "\n");
-            } else if (command.equals("empty")) {
-                if (queue.isEmpty()) {
-                    bw.write(1 + "\n");
-                } else {
-                    bw.write(0 + "\n");
-                }
-            } else if (command.equals("front")) {
-                bw.write(queue.peek() + "\n");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // 큐 선언
+        Queue<Integer> q = new LinkedList<>(); 
+        
+        for(int i = 0; i < n; i++) {
+            String command = sc.next();
+
+            if(command.equals("push")) {
+                int x = sc.nextInt();
+                q.add(x);
+            }
+            else if(command.equals("pop")) {
+                System.out.println(q.poll());
+            }
+            else if(command.equals("size")) {
+                System.out.println(q.size());
+            }
+            else if(command.equals("empty")) {
+                if(q.isEmpty())
+                    System.out.println(1);
+                else
+                    System.out.println(0);
+            }
+            else {
+                System.out.println(q.peek());
             }
         }
-        bw.flush();
-        bw.close();
-        br.close();
     }
 }
