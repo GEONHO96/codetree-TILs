899bf198acbd5588189d0d28e296b49f06e70142
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-04 11:24:39 +0900

    Title: 정수 명령 처리 2, Time: 144ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250304/정수 명령 처리 2/process-numeric-commands-2.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Queue<Integer> queue = new LinkedList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            if (command.equals("push")) {
+                int K = Integer.parseInt(st.nextToken());
+                queue.add(K);
+            }
+            if (command.equals("pop")) {
+                bw.write(queue.poll() + "\n");
+            }
+            if (command.equals("size")) {
+                bw.write(queue.size() + "\n");
+            }
+            if (command.equals("size")) {
+                if (queue.isEmpty()) {
+                    bw.write(1 + "\n");
+                } else {
+                    bw.write(0 + "\n");
+                }
+            }
+            if (command.equals("front")) {
+                bw.write(queue.peek() + "\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
