63bf39527da384f403cb553e6acd1f0ad07917f4
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-27 12:09:49 +0900

    Title: 가장 많은 데이터, Time: 356ms, Memory: 22MB, Status: Passed - Codetree

--- 250327/가장 많은 데이터/most-frequent-data.java
+++ 250327/가장 많은 데이터/most-frequent-data.java
@@ -1,38 +1,28 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.HashMap;
 
-public class Main {    
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static int n;
-    public static String[] words = new String[MAX_N];
-    public static HashMap<String, Integer> freq = new HashMap<>();
-    
-    public static int ans; // 가장 많이 나온 횟수를 기록합니다.
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-
-        for(int i = 0; i < n; i++)
-            words[i] = sc.next();
-        
-        // 각 문자열이 몇 번씩 나왔는지를
-        // hashmap에 기록해줍니다.
-        for(int i = 0; i < n; i++) {
-            // 처음 나온 문자열이라면 1을 직접 적어줘야 합니다.
-            if(!freq.containsKey(words[i]))
-                freq.put(words[i], 1);
-            // 이미 나와있던 문자열이라면 1을 더해줍니다.
-            else
-                freq.put(words[i], freq.get(words[i]) + 1);
-            
-            // 최댓값을 갱신합니다.
-            ans = Math.max(ans, freq.get(words[i]));
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<>();
+        int maxCount = 0;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            int count = map.getOrDefault(str, 0) + 1;
+            map.put(str, count);
+            if (count > maxCount) {
+                maxCount = count;
+            }
         }
-
-        System.out.print(ans);
+        bw.write(maxCount + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
