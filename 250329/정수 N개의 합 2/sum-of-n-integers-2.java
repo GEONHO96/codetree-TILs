5f74bba83b4f8c6f3c4ea3c101235eb1ec81f7af
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-03-29 23:30:35 +0900

    Title: 정수 N개의 합 2, Time: 232ms, Memory: 19MB, Status: Passed - Codetree

--- 250329/정수 N개의 합 2/sum-of-n-integers-2.java
+++ 250329/정수 N개의 합 2/sum-of-n-integers-2.java
@@ -1,38 +1,34 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main {    
-    public static final int INT_MIN = Integer.MIN_VALUE;
-    public static final int MAX_N = 100000;
-    
-    // 변수 선언
-    public static int n, k;
-    public static int[] arr = new int[MAX_N + 1];
-    public static int[] prefixSum = new int[MAX_N + 1];
-    public static int ans = INT_MIN;
-    
-    // [s, e] 구간 내의 원소의 합을 반환합니다.
-    public static int getSum(int s, int e) {
-        return prefixSum[e] - prefixSum[s - 1];
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-        k = sc.nextInt();
-        for(int i = 1; i <= n; i++)
-            arr[i] = sc.nextInt();
-
-        // 누적합 배열을 만들어줍니다.
-        prefixSum[0] = 0;
-        for(int i = 1; i<= n; i++)
-            prefixSum[i] = prefixSum[i - 1] + arr[i];
-        
-        // 모든 구간에 대해 합을 찾아
-        // 그 중 최댓값을 갱신합니다.
-        for(int i = 1; i <= n - k + 1; i++)
-            ans = Math.max(ans, getSum(i, i + k - 1));
-
-        System.out.print(ans);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += arr[i];
+        }
+        int max = sum;
+        for (int i = K; i < N; i++) {
+            sum = sum - arr[i - K] + arr[i];
+            max = Math.max(max, sum);
+        }
+        bw.write(String.valueOf(max) + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
