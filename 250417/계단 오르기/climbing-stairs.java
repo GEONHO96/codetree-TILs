1a70935da38d8dc70acf0c10d8f432c15bb89c55
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-17 21:54:47 +0900

    Title: 계단 오르기, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

--- 250417/계단 오르기/climbing-stairs.java
+++ 250417/계단 오르기/climbing-stairs.java
@@ -1,30 +1,25 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static final int MOD = 10007;
-    public static final int MAX_N = 1000;
-    
-    // 변수 선언
-    public static int n;
-    
-    public static int[] dp = new int[MAX_N + 1];
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력:
-        n = sc.nextInt();
-
-        // 초기 조건 설정
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[Math.max(N + 1, 4)];
         dp[0] = 1;
         dp[1] = 0;
         dp[2] = 1;
         dp[3] = 1;
-
-        // 점화식에 따라 dp값 채우기
-        // dp[i] = dp[i - 2] + dp[i - 3]
-        for(int i = 4; i <= n; i++)
-            dp[i] = (dp[i - 2] + dp[i - 3]) % MOD;
-
-        System.out.print(dp[n]);
+        for (int i = 4; i <= N; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 3]) % 10007;
+        }
+        bw.write(dp[N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
