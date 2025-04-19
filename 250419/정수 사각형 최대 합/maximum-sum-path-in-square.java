18726124c4ebb6f5081351b791ec294d9dbad924
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-19 19:41:17 +0900

    Title: 정수 사각형 최대 합, Time: 140ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250419/정수 사각형 최대 합/maximum-sum-path-in-square.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] board = new int[N + 1][N + 1];
+        int[][] dp = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + board[i][j];
+            }
+        }
+        bw.write(dp[N][N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
