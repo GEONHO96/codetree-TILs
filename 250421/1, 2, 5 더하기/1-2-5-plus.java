53cd273d8ae19620e881161f6b4330f6ac2efc96
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-21 19:18:38 +0900

    Title: 1, 2, 5 더하기, Time: 97ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250421/1, 2, 5 더하기/1-2-5-plus.java
+++ 250421/1, 2, 5 더하기/1-2-5-plus.java
@@ -12,11 +12,11 @@ public class Main {
         int[] dp = new int[N + 1];
         dp[0] = 1;
         for (int i = 1; i <= N; i++) {
-            if (i >= 1) dp[i] += dp[i - 1] % 10007;
-            if (i >= 2) dp[i] += dp[i - 2] % 10007;
-            if (i >= 5) dp[i] += dp[i - 5] % 10007;
+            if (i >= 1) dp[i] += dp[i - 1];
+            if (i >= 2) dp[i] += dp[i - 2];
+            if (i >= 5) dp[i] += dp[i - 5];
         }
-        bw.write(dp[N] + "\n");
+        bw.write(dp[N] % 10007 + "\n");
         bw.flush();
         bw.close();
         br.close();
