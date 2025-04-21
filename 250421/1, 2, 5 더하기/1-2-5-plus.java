0b898fec29d45fda16e7772c485766c542599219
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-04-21 19:17:30 +0900

    Title: 1, 2, 5 더하기, Time: 90ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250421/1, 2, 5 더하기/1-2-5-plus.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= N; i++) {
+            if (i >= 1) dp[i] += dp[i - 1];
+            if (i >= 2) dp[i] += dp[i - 2];
+            if (i >= 5) dp[i] += dp[i - 5];
+        }
+        bw.write(dp[N] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
