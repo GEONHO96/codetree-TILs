2aef45570700d979281b89f7de151ead70670dec
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-01 14:06:21 +0900

    Title: 1, 2, 5 더하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250501/1, 2, 5 더하기/1-2-5-plus.java
+++ 250501/1, 2, 5 더하기/1-2-5-plus.java
@@ -1,27 +1,39 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class Main {
-    static int MOD = 10_007;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        long[] dp = new long[N + 1];
+    public static final int MOD = 10007;
+    public static final int MAX_M = 3;
+    public static final int MAX_N = 1000;
+
+    // 변수 선언
+    public static int n;
+
+    // dp[i] : 합 i를 만들기 위한 서로 다른 가짓수
+    public static int[] dp = new int[MAX_N + 1];
+    public static int[] numbers = new int[]{1, 2, 5};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+
+        // 초기 조건은
+        // 아직 아무 수도 고르지 않았을 때이므로
+        // 합 0을 만들기 위한 가짓수가 1이 되어
+        // dp[0] = 1입니다.
         dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 5;
-        for (int i = 5; i <= N; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 5]) % MOD;
+
+        // 점화식에 따라 값을 채워줍니다.
+        for(int i = 1; i <= n; i++) {
+            // 마지막으로 고른 숫자가 numbers[j]인 경우에 대해
+            // 가짓수를 더해줍니다.
+            for(int j = 0; j < MAX_M; j++) {
+                if(i >= numbers[j])
+                    dp[i] = (dp[i] + dp[i - numbers[j]]) % MOD;
+            }
         }
-        bw.write(dp[N] + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+
+        // 합이 n이 되기 위한 가짓수를 출력합니다.
+        System.out.print(dp[n]);
     }
 }
