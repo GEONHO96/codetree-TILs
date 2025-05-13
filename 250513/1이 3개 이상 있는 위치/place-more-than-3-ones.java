a3dba34cb58748511d25edb2559fc53b1235d3b6
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-13 09:49:35 +0900

    Title: 1이 3개 이상 있는 위치, Time: 108ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250513/1이 3개 이상 있는 위치/place-more-than-3-ones.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] arr;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static boolean inRange(int x, int y) {
+        return (0 <= x && x < N && 0 <= y && y < N);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int cnt = 0;
+                for (int k = 0; k < 4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (inRange(nx, ny) && arr[nx][ny] == 1) cnt++;
+                }
+                if (cnt >= 3) ans++;
+            }
+        }
+        bw.write(ans + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
