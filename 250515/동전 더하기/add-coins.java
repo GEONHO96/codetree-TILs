ca505548362b41082799f50dbc2cc27912f39657
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-05-15 14:11:18 +0900

    Title: 동전 더하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250515/동전 더하기/add-coins.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int count = 0;
+        Integer[] coins = new Integer[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(coins, Collections.reverseOrder());
+        while (K > 0) {
+            for (int i = 0; i < N; i++) {
+                count += K / coins[i];
+                K %= coins[i];
+            }
+        }
+        bw.write(count + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
