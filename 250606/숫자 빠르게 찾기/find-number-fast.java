31eb37b5c91d7f5520b2be35d622a76e147cdad1
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-06-06 09:24:36 +0900

    Title: 숫자 빠르게 찾기, Time: 462ms, Memory: 22MB, Status: Passed - Codetree

--- 250606/숫자 빠르게 찾기/find-number-fast.java
+++ 250606/숫자 빠르게 찾기/find-number-fast.java
@@ -1,45 +1,42 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main {    
-    public static final int MAX_N = 100000;
-    
-    public static int n, m;
-    public static int[] arr = new int[MAX_N];
-    
-    public static int find(int target) {
-        int left = 0, right = n - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if(arr[mid] == target)
-                return mid;
-            
-            if(arr[mid] > target)
-                right = mid - 1;
-            else
-                left = mid + 1;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-    
-        return -1;
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // 입력
-        n = sc.nextInt();
-        m = sc.nextInt();
-        for(int i = 0; i < n; i++)
-            arr[i] = sc.nextInt();
-
-        for(int i = 0; i < m; i++) {
-            int x = sc.nextInt();
-
-            // 이진탐색을 진행합니다.
-            int index = find(x);
-
-            if(index >= 0)
-                System.out.println(index + 1);
-            else
-                System.out.println(-1);
+        for (int i = 0; i < M; i++) {
+            int idx = -1;
+            int target = Integer.parseInt(br.readLine());
+            int left = 0, right = N - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (arr[mid] == target) {
+                    idx = mid + 1;
+                    break;
+                }
+                if (arr[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            bw.write(idx + "\n");
         }
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
