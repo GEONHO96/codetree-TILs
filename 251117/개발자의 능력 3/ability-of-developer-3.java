f20327790a0ef95f55d65f7db57feae489f5eb7f
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-17 10:43:56 +0900

    Title: 개발자의 능력 3, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251117/개발자의 능력 3/ability-of-developer-3.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] ability = new int[6];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 6; i++) {
+            ability[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ability);
+        int diff = Math.abs((ability[0] + ability[2] + ability[5]) - (ability[1] + ability[3] + ability[4]));
+        bw.write(String.valueOf(diff));
+        bw.flush();
+        bw.close();
+        br.close(); 
+    }
+}
