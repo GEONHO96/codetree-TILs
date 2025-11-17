7c00ce331c0dc5733c2181ec1e54a371d596ee3c
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-17 10:22:34 +0900

    Title: 규칙에 따라 밀기, Time: 98ms, Memory: 12MB, Status: Passed - Codetree

--- 251117/규칙에 따라 밀기/push-by-the-rules.java
+++ 251117/규칙에 따라 밀기/push-by-the-rules.java
@@ -1,29 +1,23 @@
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        // 문자열을 입력받습니다.
-        String str = sc.next();
-        String str2 = sc.next();
-
-        // 문자열의 길이를 구합니다.
-        int len = str.length();
-
-        // 명령 문자열에서 제시하는 대로 행동합니다.
-        for(int i = 0; i < str2.length(); i++) {
-            if (str2.charAt(i) == 'L') {
-                // 명령 문자열이 L일 때에는 문자열을 왼쪽으로 한 칸 쉬프트합니다.
-                str = str.substring(1) + str.substring(0, 1);
-            }
-            else {
-                // 명령 문자열이 R일 때에는 문자열을 오른쪽으로 한 칸 쉬프트합니다.
-                str = str.substring(len - 1) + str.substring(0, len - 1);
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String A = br.readLine();
+        String command = br.readLine();
+        int length = command.length();
+        for (int i = 0; i < length; i++) {
+            if (command.charAt(i) == 'L') A = A.substring(1, A.length()) + A.charAt(0);
+            if (command.charAt(i) == 'R') A = A.charAt(A.length() - 1) + A.substring(0, A.length() - 1);                        
         }
-
-        // 명령대로 쉬프트된 문자열을 출력합니다.
-        System.out.println(str);
+        bw.write(A);
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
