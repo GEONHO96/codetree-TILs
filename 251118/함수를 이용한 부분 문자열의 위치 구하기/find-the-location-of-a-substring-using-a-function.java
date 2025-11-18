2aeeb3f4140db498ab8a99cb4bdc91f5d7ae620b
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-18 13:12:22 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 81ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 251118/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String S = br.readLine();
+        int N = S.length();
+        String T = br.readLine();
+        int M = T.length();
+        int idx = -1;
+        for (int i = 0; i < N - M + 1; i++) {
+            if (S.substring(i, i + M).equals(T)) idx = i;
+        }
+        bw.write(String.valueOf(idx));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
