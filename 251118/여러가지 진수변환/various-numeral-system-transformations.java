1794774567e55d52354a055d8f51ec291062f4b7
이건호 <117559957+GEONHO96@users.noreply.github.com>
2025-11-18 11:11:09 +0900

    Title: 여러가지 진수변환, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

--- 251118/여러가지 진수변환/various-numeral-system-transformations.java
+++ 251118/여러가지 진수변환/various-numeral-system-transformations.java
@@ -1,21 +1,31 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        String S = Integer.toString(N, B);
-        bw.write(S);
-        bw.flush();
-        bw.close();
-        br.close();
+    public static final int MAX_DIGIT = 20;
+
+    public static int n, b;
+    public static int[] digits = new int[MAX_DIGIT];
+    public static int cnt;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력
+        n = sc.nextInt();
+        b = sc.nextInt();
+
+        // b진수로 변환합니다.
+        while(true) {
+            if (n < b) {
+                digits[cnt++] = n;
+                break;
+            }
+
+            digits[cnt++] = n % b;
+            n /= b;
+        }
+
+        // 진수 배열을 뒤집어 b진수를 출력합니다.
+        for(int i = cnt - 1; i >= 0; i--)
+            System.out.print(digits[i]);
     }
 }
