1c75193918aeed67f754b1f18a228ac5ce01ae64
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-01-11 12:24:15 +0900

    Title: 정수 찾기, Time: 512ms, Memory: 28MB, Status: Passed - Codetree

---
+++ 260111/정수 찾기/find-an-integer.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        HashMap<Integer, Integer> A = new HashMap<>();
+        HashMap<Integer, Integer> B = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            A.put(num, 1);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            B.put(num, 1);
+            bw.write(A.containsKey(num) ? "1\n" : "0\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
