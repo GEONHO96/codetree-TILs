a496274142b162d80b53c0da68e1ff1e54bacbfe
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-02-16 23:55:58 +0900

    Title: 서로 다른 수의 개수, Time: 237ms, Memory: 22MB, Status: Passed - Codetree

---
+++ 260216/서로 다른 수의 개수/distinct-numbers.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> s = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            s.add(num);
+        }
+        Integer[] arr = s.toArray(new Integer[0]);
+        bw.write(String.valueOf(arr.length));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
