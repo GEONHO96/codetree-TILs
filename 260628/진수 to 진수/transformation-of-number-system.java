f0fbc2284c0655f1926d790c2d731b313fc4da68
이건호 <117559957+GEONHO96@users.noreply.github.com>
2026-06-28 15:49:37 +0900

    [Passed] 진수 to 진수 (107ms, 9MB)

---
+++ 260628/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(String.valueOf(N), A);
+        String S = Integer.toString(C, B);
+        bw.write(String.valueOf(S));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
